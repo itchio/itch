ab2cb9497605c95df9af838101324fb2e5114b3d
Amos Wenger <fasterthanlime@gmail.com>
2015-10-02 15:54:00 +0200

    windows .ico, switch to grunt-electron to create custom exe

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,48 @@
 
-module.exports = function (grunt) {
+var fs = require('fs');
+var path = require('path');
+var license_path = path.join(__dirname, 'LICENSE');
+var license = fs.readFileSync(license_path, { encoding: 'utf8' });
+var package_path = path.join(__dirname, 'app', 'package.json');
+var version = JSON.parse(fs.readFileSync(package_path, { encoding: 'utf8' })).version;
 
+module.exports = function (grunt) {
   grunt.initConfig({
-    'build-electron-app': {
-      options: {
-        platforms: ['win32']
+    'electron': {
+      win32Build: {
+        options: {
+          dir: 'app',
+          name: 'itch.io',
+          platform: 'win32',
+          arch: 'ia32',
+          version: '0.33.4',
+          out: 'build',
+          icon: 'app/static/images/itchio.ico',
+          asar: true,
+          'app-version': version,
+          'version-string': {
+            CompanyName: 'itch corp',
+            LegalCopyright: license,
+            FileDescription: 'itch.io desktop client',
+            OriginalFileName: 'itch.io.exe',
+            FileVersion: version,
+            AppVersion: version,
+            ProductName: 'itch.io',
+            InternalName: 'itch.io.exe'
+          }
+        }
       }
     },
     'create-windows-installer': {
       ia32: {
-        appDirectory: 'build/win32',
+        appDirectory: 'build/itch.io-win32-ia32',
         outputDirectory: 'build/installer-win32',
         authors: 'itch corp',
-        exe: 'electron.exe'
+        exe: 'itch.io.exe'
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-electron-app-builder');
-  grunt.loadNpmTasks('grunt-electron-installer');
+  require('load-grunt-tasks')(grunt);
 }
 
