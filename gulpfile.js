094f90d553bdb9579aeda230a1e23639009b5aaa
Amos Wenger <fasterthanlime@gmail.com>
2015-10-07 20:37:04 +0200

    :lipstick: Apply standard to gulpfile & test harness as well

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,83 @@
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var gutil = require('gulp-util');
-var babel = require('gulp-babel');
-var plumber = require('gulp-plumber');
-var streamify = require('gulp-streamify');
-var concat = require('gulp-concat');
-var watchify = require('gulp-watchify');
-var sourcemaps = require('gulp-sourcemaps');
-var babelify = require('babelify');
-var envify = require('envify');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var babel = require('gulp-babel')
+var plumber = require('gulp-plumber')
+var streamify = require('gulp-streamify')
+var concat = require('gulp-concat')
+var watchify = require('gulp-watchify')
+var sourcemaps = require('gulp-sourcemaps')
+var babelify = require('babelify')
+var envify = require('envify')
 
 var paths = {
   chrome: ['./app/chrome/main.es6'],
   metal: ['./app/main.es6', './app/metal/**/*.es6'],
   scss: ['./app/style/**/*.scss']
-};
+}
 
 /*
  * Compile and bundle chrome code with watchify
  */
 
-var watching = false;
+var watching = false
 
-gulp.task('enable-watch-mode', function() {
-  watching = true;
-});
+gulp.task('enable-watch-mode', function () {
+  watching = true
+})
 
-gulp.task('chrome', watchify(function(watchify) {
+gulp.task('chrome', watchify(function (watchify) {
   return gulp.src(paths.chrome).pipe(plumber()).pipe(watchify({
     watch: watching,
     extensions: ['.es6', '.js'],
     debug: true,
-    setup: function(bundle) {
-      bundle.transform(babelify);
-      return bundle.transform(envify);
+    setup: function (bundle) {
+      bundle.transform(babelify)
+      return bundle.transform(envify)
     }
   }))
   .pipe(streamify(sourcemaps.init({loadMaps: true})))
   .pipe(streamify(concat('bundle.js')))
   .pipe(streamify(sourcemaps.write('./maps')))
-  .pipe(gulp.dest('./app/chrome/'));
-}));
-
+  .pipe(gulp.dest('./app/chrome/'))
+}))
 
 /*
  * Compile metal code in-place with babel
  */
 
-gulp.task('metal', function() {
+gulp.task('metal', function () {
   return gulp.src(paths.metal, {
     base: './app/'
   }).pipe(plumber())
   .pipe(sourcemaps.init())
   .pipe(babel())
   .pipe(sourcemaps.write('./maps'))
-  .pipe(gulp.dest('./app/'));
-});
-
+  .pipe(gulp.dest('./app/'))
+})
 
 /*
  * Compile css with sass
  */
 
-gulp.task('scss', function() {
+gulp.task('scss', function () {
   return gulp.src('./app/style/main.scss')
   .pipe(plumber())
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./app/style'));
-});
-
+  .pipe(gulp.dest('./app/style'))
+})
 
 /*
  * Watch all code changes and recompile on-demand
  */
 
-gulp.task('watch', ['enable-watch-mode'], function() {
-  gulp.watch(paths.metal, ['metal']);
-  gulp.watch(paths.scss, ['scss']);
-  return gulp.start('chrome');
-});
+gulp.task('watch', ['enable-watch-mode'], function () {
+  gulp.watch(paths.metal, ['metal'])
+  gulp.watch(paths.scss, ['scss'])
+  return gulp.start('chrome')
+})
 
-gulp.task('all', ['metal', 'chrome', 'scss']);
+gulp.task('all', ['metal', 'chrome', 'scss'])
 
-gulp.task('default', ['all']);
+gulp.task('default', ['all'])
 
--- spec/chrome/static/main.js
+++ spec/chrome/static/main.js
@@ -1,65 +1,60 @@
 
 // Mostly stolen from https://github.com/atom/electron/tree/master/spec
 
-'use strict';
+'use strict'
 
-var app = require('app');
-var ipc = require('ipc');
-var dialog = require('dialog');
-var path = require('path');
-var BrowserWindow = require('browser-window');
+var app = require('app')
+var ipc = require('ipc')
+var dialog = require('dialog')
+var BrowserWindow = require('browser-window')
 
-var window = null;
+var window = null
 
 // Accessing stdout in the main process will result in the process.stdout
 // throwing UnknownSystemError in renderer process sometimes. This line makes
 // sure we can reproduce it in renderer process.
-process.stdout;
+process.stdout
 
-ipc.on('message', function(event, arg) {
-  event.sender.send('message', arg);
-});
+ipc.on('message', function (event, arg) {
+  event.sender.send('message', arg)
+})
 
-ipc.on('console.log', function(event, args) {
-  console.error.apply(console, args);
-});
+ipc.on('console.log', function (event, args) {
+  console.error.apply(console, args)
+})
 
-ipc.on('console.error', function(event, args) {
-  console.error.apply(console, args);
-});
+ipc.on('console.error', function (event, args) {
+  console.error.apply(console, args)
+})
 
-ipc.on('process.exit', function(event, code) {
+ipc.on('process.exit', function (event, code) {
   // on Windows, calling process.exit(code) here will always
   // return 3221226356 (heap corruption apparently?) so we
   // call app.quit() on success instead, which actually results
   // in a 0 exit code.
   // -- @fasterthanlime, spent a day debugging various related issues
-  if (code == 0) {
-    app.quit();
+  if (code === 0) {
+    app.quit()
   } else {
-    process.exit(code);
+    process.exit(code)
   }
-});
+})
 
-ipc.on('eval', function(event, script) {
-  event.returnValue = eval(script);
-});
+ipc.on('echo', function (event, msg) {
+  event.returnValue = msg
+})
 
-ipc.on('echo', function(event, msg) {
-  event.returnValue = msg;
-});
-
-if (process.argv[2] == '--ci') {
-  process.removeAllListeners('uncaughtException');
-  process.on('uncaughtException', function(error) {
-    console.error(error, error.stack);
-    process.exit(1);
-  });
+if (process.argv[2] === '--ci') {
+  process.removeAllListeners('uncaughtException')
+  process.on('uncaughtException', function (error) {
+    console.error(error, error.stack)
+    process.exit(1)
+  })
 }
 
-app.on('window-all-closed', function() {
-  app.quit();
-});
+app.on('window-all-closed', function () {
+  app.quit()
+})
 
 app.on('ready', function () {
   window = new BrowserWindow({
@@ -67,15 +62,15 @@ app.on('ready', function () {
     show: false,
     width: 800,
     height: 600
-  });
-  window.loadUrl('file://' + __dirname + '/index.html');
-  window.on('unresponsive', function() {
+  })
+  window.loadUrl('file://' + __dirname + '/index.html')
+  window.on('unresponsive', function () {
     var chosen = dialog.showMessageBox(window, {
       type: 'warning',
       buttons: ['Close', 'Keep Waiting'],
       message: 'Window is not responsing',
       detail: 'The window is not responding. Would you like to force close it or just keep waiting?'
-    });
-    if (chosen == 0) window.destroy();
-  });
-});
+    })
+    if (chosen === 0) window.destroy()
+  })
+})
