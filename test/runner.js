ce948a71711761a79fba5b42f732b7dcae6f3b2a
Amos Wenger <fasterthanlime@gmail.com>
2015-12-09 23:30:28 +0100

    clean up test runner

--- test/runner.js
+++ test/runner.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
+(function () {
+  'use strict'
 
-var path = require('path');
-var glob = require('glob');
-require('source-map-support').install();
-require('nodent')();
+  var path = require('path')
+  var glob = require('glob')
+  require('source-map-support').install()
+  require('nodent')()
 
-process.argv.slice(2).forEach(function (arg) {
-    glob(arg, function (err, files) {
-        files.forEach(function (file) {
-            var test = path.resolve(process.cwd(), file);
-            console.log("> " + test);
-            require(test);
-        });
-    });
-});
-
-// vim: ft=javascript
+  process.argv.slice(2).forEach((arg) => {
+    glob(arg, function (e, files) {
+      files.forEach(function (file) {
+        let test = path.resolve(process.cwd(), file)
+        console.log('> ' + test)
+        require(test)
+      })
+    })
+  })
+})()
