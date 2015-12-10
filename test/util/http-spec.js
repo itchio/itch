ade8c265fb671e787d53167307fcab47ccbd3391
Amos Wenger <fasterthanlime@gmail.com>
2015-12-11 00:43:34 +0100

    better http spec

--- test/util/http-spec.js
+++ test/util/http-spec.js
@@ -2,55 +2,32 @@
 
 let test = require('zopf')
 let proxyquire = require('proxyquire')
-let Promise = require('bluebird')
-
-let PassThrough = require('stream').PassThrough
-let EventEmitter = require('events').EventEmitter
+let sinon = require('sinon')
 
 let electron = require('../stubs/electron')
 
 test('http', t => {
+  let onprogress = t.spy()
   let http_opts = {
     url: 'http://-invalid/hello.txt',
     dest: '/dev/null',
-    onprogress: () => {}
+    onprogress
   }
 
-  let child
-
-  let child_process = {
-    spawn: () => {
-      child = Object.assign({
-        stdout: new PassThrough()
-      }, EventEmitter.prototype)
-      return child
-    },
-    mkdirp: () => Promise.resolve(),
-    '@global': true
-  }
-
+  let spawn = t.stub().resolves(42)
   let stubs = Object.assign({
-    'child_process': child_process
+    './spawn': spawn
   }, electron)
 
   let http = proxyquire('../../app/util/http', stubs)
 
-  t.case('resolves on close', t => {
-    setTimeout(() => { child.emit('close') }, 10)
-    return http.request(http_opts)
-  })
+  t.case('spawns butler', async t => {
+    let r = await http.request(http_opts)
+    sinon.assert.calledOnce(spawn)
+    t.is(r, 42)
 
-  t.case('rejects error', t => {
-    setTimeout(() => { child.emit('error') }, 10)
-    return t.rejects(http.request(http_opts))
-  })
-
-  t.case('emits progress', t => {
-    setTimeout(() => {
-      child.stdout.write(JSON.stringify({type: 'progress', percentage: 39}) + '\n')
-      child.emit('close')
-    }, 10)
-    t.mock(http_opts).expects('onprogress').withArgs({percent: 39})
-    return http.request(http_opts)
+    let ontoken = spawn.getCall(0).args[0].ontoken
+    ontoken(JSON.stringify({type: 'progress', percentage: 45.12}))
+    sinon.assert.calledWith(onprogress, {percent: 45.12})
   })
 })
