06569afa2a5cd390b673db68dd52e4583a4fcde7
Amos Wenger <fasterthanlime@gmail.com>
2015-12-12 16:51:34 +0100

    configure grunt-bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,6 +107,25 @@ module.exports = function (grunt) {
         command: "babel -D -d stage/app app"
       },
     },
+    'bump': {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: ':arrow_up: v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: false,
+        metadata: '',
+        regExp: false
+      }
+    },
   })
 
   grunt.registerTask('default', ['shell:sass']);
