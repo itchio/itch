2006b075a765a3f1a570cb302530ebf4860c290a
Amos Wenger <fasterthanlime@gmail.com>
2015-12-13 18:49:59 +0100

    fix shortcut creation on windows

--- app/util/reg.js
+++ app/util/reg.js
@@ -15,8 +15,9 @@ let system_root = process.env.SystemRoot || 'missing-system-root'
 let system32_path = path.join(system_root, 'System32')
 let reg_path = path.join(system32_path, 'reg.exe')
 
-let root_folder = path.resolve(process.execPath, '..')
-let squirrel_exe_path = path.join(root_folder, 'squirrel.exe')
+let app_folder = path.resolve(process.execPath, '..')
+let root_folder = path.resolve(app_folder, '..')
+let update_exe_path = path.join(root_folder, 'Update.exe')
 let exe_name = path.basename(process.execPath)
 
 let self = {
@@ -50,16 +51,16 @@ let self = {
     })
   },
 
-  squirrel_run: async function (args) {
+  update_run: async function (args) {
     await spawn({
-      command: squirrel_exe_path,
+      command: update_exe_path,
       args
     })
   },
 
   install: async function (opts) {
     try {
-      await self.squirrel_run(['--createShortcut', exe_name])
+      await self.update_run(['--createShortcut', exe_name])
       await self.reg_add_default(base, 'URL:itch.io protocol')
       await self.reg_add_empty(base, 'URL protocol')
       await self.reg_add_default(`${base}\\DefaultIcon`, 'itch.exe')
@@ -71,7 +72,7 @@ let self = {
 
   uninstall: async function () {
     try {
-      await self.squirrel_run(['--removeShortcut', exe_name])
+      await self.update_run(['--removeShortcut', exe_name])
       await self.reg_delete_all(base)
     } catch (e) {
       log(opts, `Could not register itchio:// as default protocol handler: ${e.stack || e}`)
