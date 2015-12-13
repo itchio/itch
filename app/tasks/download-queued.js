836e3ef7847e8a83cce8191fdf2ffa62bca7e4d0
Amos Wenger <fasterthanlime@gmail.com>
2015-12-13 15:19:28 +0100

    debug--

--- app/tasks/download-queued.js
+++ app/tasks/download-queued.js
@@ -4,8 +4,6 @@ let Promise = require('bluebird')
 let Transition = require('./errors').Transition
 
 function start (opts) {
-  console.log(`download-queue got opts:\n${JSON.stringify(opts, null, 2)}`)
-
   return new Promise((resolve, reject) => {
     opts.emitter.on('shine', t => {
       reject(new Transition({
