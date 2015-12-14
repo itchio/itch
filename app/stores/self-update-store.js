8571e151c42b1340b0396bbd3f5d818dcdb2e9ed
Amos Wenger <fasterthanlime@gmail.com>
2015-12-14 03:57:44 +0100

    log some info

--- app/stores/self-update-store.js
+++ app/stores/self-update-store.js
@@ -33,9 +33,11 @@ function window_ready () {
   auto_updater.on('checking-for-update', AppActions.checking_for_self_update)
   auto_updater.on('update-available', AppActions.self_update_available)
   auto_updater.on('update-not-available', AppActions.self_update_not_available)
-  auto_updater.on('update-downloaded', (ev, release_notes, release_name) =>
+  auto_updater.on('update-downloaded', (ev, release_notes, release_name) => {
+    console.log(`update downloaded, release name: '${release_name}'`)
+    console.log(`release notes: \n'${release_notes}'`)
     AppActions.self_update_downloaded(release_name)
-  )
+  })
 }
 
 function check_for_self_update () {
