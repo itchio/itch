7f6b1fa1e832770f70733fe24f14abf0a9daef22
Matias Beckerle <matiasbeckerle@gmail.com>
2015-12-21 23:52:55 -0300

    Add SelectRow component and use it in language selector

--- app/components/forms.js
+++ app/components/forms.js
@@ -13,7 +13,6 @@ class InputRow extends Component {
 
   render () {
     let name = this.props.name
-    let default_value = this.props.defaultValue
     let label = this.props.label || ''
     let type = this.props.type || 'text'
     let disabled = this.props.disabled
@@ -22,7 +21,7 @@ class InputRow extends Component {
       r.div({className: 'input-row'}, [
         r.label({}, [
           label,
-          r.input({type, disabled, ref: 'input', placeholder: name, defaultValue: default_value})
+          r.input({type, disabled, ref: 'input', placeholder: name})
         ])
       ])
     )
@@ -41,4 +40,43 @@ InputRow.propTypes = {
   type: PropTypes.oneOf(['text', 'password'])
 }
 
-module.exports = {InputRow}
+class SelectRow extends Component {
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      value: this.props.value
+    }
+  }
+
+  handleChange (event) {
+    this.setState({value: event.target.value})
+  }
+
+  render () {
+    let options = this.props.options
+    let value = this.state.value
+    let label = this.props.label || ''
+
+    return (
+      r.div({className: 'select-row'}, [
+        r.label({}, [
+          label,
+          r.select({ref: 'input', value: value, onChange: this.handleChange}, options.map((option, index) => r.option({value: option.value}, option.label)))
+        ])
+      ])
+    )
+  }
+
+  value () {
+    return this.refs.input.value
+  }
+}
+
+SelectRow.propTypes = {
+  options: PropTypes.array.isRequired,
+  value: PropTypes.string,
+  label: PropTypes.string.isRequired
+}
+
+module.exports = { InputRow, SelectRow }
