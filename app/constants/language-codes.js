83f899ad4901e6e690092d83077f22dd9fd38ca1
Amos Wenger <fasterthanlime@gmail.com>
2015-12-25 19:49:28 +0100

    :art: Move language codes to own file

---
+++ app/constants/language-codes.js
@@ -0,0 +1,23 @@
+
+module.exports = [
+  { value: 'ar', label: 'العربية' },
+  { value: 'cs', label: 'Čeština' },
+  { value: 'de', label: 'Deutsch' },
+  { value: 'el', label: 'ελληνικά' },
+  { value: 'en', label: 'English' },
+  { value: 'es', label: 'Español' },
+  { value: 'fi', label: 'Suomi' },
+  { value: 'fr', label: 'Français' },
+  { value: 'hu', label: 'Magyar' },
+  { value: 'it', label: 'Italiano' },
+  { value: 'ja', label: '日本語' },
+  { value: 'ko', label: '한국어' },
+  { value: 'nb', label: 'Norsk (bokmål)' },
+  { value: 'nl', label: 'Nederlands Vlaams' },
+  { value: 'pl', label: 'Polszczyzna' },
+  { value: 'pt_BR', label: 'Português (do Brasil)' },
+  { value: 'ro', label: 'Română' },
+  { value: 'ru', label: 'Русский' },
+  { value: 'sv', label: 'Svenska' },
+  { value: 'zh', label: '中文' }
+]
