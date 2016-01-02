6813bdb56d21516750342d25dc1c31a80cf96ee3
Amos Wenger <fasterthanlime@gmail.com>
2016-01-02 13:31:19 +0100

    :arrow_down: :snowflake: freeze to react 0.14.3 until newer versions stop assuming we have a DOM available

--- test/stubs/react-stubs.js
+++ test/stubs/react-stubs.js
@@ -5,10 +5,20 @@ let defer = require('./defer')
 let AppStore = require('./app-store')
 let AppActions = require('./app-actions')
 
+let i18next = {
+  '@global': true,
+  use: () => i18next,
+  init: () => i18next,
+  on: () => null,
+  off: () => null,
+  t: (x) => x
+}
+
 let self = Object.assign({
   '../util/defer': defer,
   '../stores/app-store': AppStore,
-  '../actions/app-actions': AppActions
+  '../actions/app-actions': AppActions,
+  i18next
 }, electron)
 
 Object.assign(self, { defer, AppStore, AppActions })
