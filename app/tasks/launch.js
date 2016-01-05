itchio/itch

commit 90164b2195a789ae924e17cfde5a38d728af2f67

:bug: :clap: Switch to spawn for games, closes #233, report game crashes, closes #181

app/tasks/launch.js
@@ -1,11 +1,10 @@
 'use strict'
 
 let path = require('path')
-let child_process = require('child_process')
 let clone = require('clone')
-let Promise = require('bluebird')
 
 let os = require('../util/os')
+let spawn = require('../util/spawn')
 
 let log = require('../util/log')('tasks/launch')
 let configure = require('./configure')
@@ -14,36 +13,32 @@ let CaveStore = require('../stores/cave-store')
 let Crash = require('./errors').Crash
 
 let self = {
-  sh: function (exe_path, cmd, opts) {
-    return new Promise((resolve, reject) => {
-      log(opts, `sh ${cmd}`)
+  sh: async function (exe_path, command, opts) {
+    log(opts, `sh ${command}`)
 
-      // pretty weak but oh well.
-      let forbidden = [';', '&']
-      for (let bidden of forbidden) {
-        if (cmd.indexOf(bidden) >= 0) {
-          throw new Error(`Command-line contains forbidden characters: ${cmd}`)
-        }
+    // pretty weak but oh well.
+    let forbidden = [';', '&']
+    for (let bidden of forbidden) {
+      if (command.indexOf(bidden) !== -1) {
+        throw new Error(`Command-line contains forbidden characters: ${command}`)
       }
+    }
 
-      let cwd = path.dirname(exe_path)
-      log(opts, `Working directory: ${cwd}`)
+    let cwd = path.dirname(exe_path)
+    log(opts, `Working directory: ${cwd}`)
 
-      child_process.exec(cmd, {
-        stdio: [ 0, 'pipe', 'pipe' ],
-        maxBuffer: 5000 * 1024,
-        cwd
-      }, (error, stdout, stderr) => {
-        if (error) {
-          log(opts, `${exe_path} returned ${error}`)
-          log(opts, `stdout:\n${stdout}`)
-          log(opts, `stderr:\n${stderr}`)
-          reject(new Crash({ exe_path, error }))
-        } else {
-          resolve(`Done playing ${exe_path}!`)
-        }
-      })
+    let res = await spawn({
+      command,
+      ontoken: (tok) => log(opts, `stdout: ${tok}`),
+      onerrtoken: (tok) => log(opts, `stderr: ${tok}`),
+      opts: { cwd }
     })
+
+    if (res.code !== 0) {
+      let error = `process exited with code ${res.code}`
+      throw new Crash({ exe_path, error })
+    }
+    return `child completed successfully`
   },
 
   escape: function (arg) {

app/util/spawn.js
@@ -15,12 +15,13 @@ function spawn (opts) {
 
   let emitter = opts.emitter
   let command = opts.command
+  let spawn_opts = opts.opts || {}
   let args = opts.args || []
   let split = opts.split || '\n'
-  let ontoken = opts.ontoken || noop
+
   log(opts, `spawning ${command} with args ${args.join(' ')}`)
 
-  let child = child_process.spawn(command, args)
+  let child = child_process.spawn(command, args, spawn_opts)
 
   if (emitter) {
     emitter.on('cancel', (e) => {
@@ -33,9 +34,17 @@ function spawn (opts) {
     })
   }
 
-  let splitter = child.stdout.pipe(StreamSplitter(split))
-  splitter.encoding = 'utf8'
-  splitter.on('token', ontoken)
+  if (opts.ontoken) {
+    let splitter = child.stdout.pipe(StreamSplitter(split))
+    splitter.encoding = 'utf8'
+    splitter.on('token', opts.ontoken)
+  }
+
+  if (opts.onerrtoken) {
+    let splitter = child.stderr.pipe(StreamSplitter(split))
+    splitter.encoding = 'utf8'
+    splitter.on('token', opts.onerrtoken)
+  }
 
   return new Promise((resolve, reject, onCancel) => {
     child.on('close', resolve)

commit 89d38f7db6ff5192876f7fc94480cca47208cf30

:bug: fix bug introduced in 90164b2195a789ae924e17cfde5a38d728af2f67 that marked games as broken

app/tasks/launch.js
@@ -31,7 +31,7 @@ let self = {
     let args = shell_quote.parse(full_command)
     let command = args.shift()
 
-    let res = await spawn({
+    let code = await spawn({
       command,
       args,
       ontoken: (tok) => log(opts, `stdout: ${tok}`),
@@ -39,8 +39,8 @@ let self = {
       opts: { cwd }
     })
 
-    if (res.code !== 0) {
-      let error = `process exited with code ${res.code}`
+    if (code !== 0) {
+      let error = `process exited with code ${code}`
       throw new Crash({ exe_path, error })
     }
     return `child completed successfully`