18d80601f6a6239eb1edae19d4d69807b4061535
Amos Wenger <fasterthanlime@gmail.com>
2016-01-06 14:51:23 +0100

    :penguin: Localize itch.desktop file - closes #314

---
+++ release/generate-itch-desktop.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+SCRIPTPATH=$( cd $( dirname -- "$0" ) > /dev/null ; pwd )
+
+DST=${SCRIPTPATH}/itch.desktop
+cat ${SCRIPTPATH}/itch.desktop.in > $DST
+
+for LOCALE_FILE in ${SCRIPTPATH}/../app/static/locales/*.json; do
+  LOCALE=$(basename $LOCALE_FILE | sed 's/.json$//')
+  STRING=$(grep "desktop.shortcut.comment" < $LOCALE_FILE | cut -d '"' -f 4)
+  if [ -n "$STRING" ]; then
+    echo "Comment[$LOCALE]=\"$STRING\"" >> $DST
+  fi
+done
