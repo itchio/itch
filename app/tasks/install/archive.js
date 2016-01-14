090cbbee82dcd143b028cde3086a746e79e2f1ed
Amos Wenger <fasterthanlime@gmail.com>
2016-01-14 16:22:44 +0100

    Split 'deploy' util out of archive

--- app/tasks/install/archive.js
+++ app/tasks/install/archive.js
@@ -1,15 +1,11 @@
 
-let _ = require('underscore')
-let Promise = require('bluebird')
-let path = require('path')
-
 let subprogress = require('../../util/subprogress')
 let sniff = require('../../util/sniff')
 let noop = require('../../util/noop')
 
-let sf = require('../../util/sf')
 let butler = require('../../util/butler')
 let extract = require('../../util/extract')
+let deploy = require('../../util/deploy')
 
 let core = require('./core')
 
@@ -52,11 +48,10 @@ let self = {
   install: async function (opts) {
     let archive_path = opts.archive_path
     let dest_path = opts.dest_path
-    let emitter = opts.emitter
 
     let onprogress = opts.onprogress || noop
     let extract_onprogress = subprogress(onprogress, 0, 80)
-    let stagecp_onprogress = subprogress(onprogress, 80, 100)
+    let deploy_onprogress = subprogress(onprogress, 80, 100)
 
     let stage_path = opts.archive_path + '-stage'
     await butler.wipe(stage_path)
@@ -72,99 +67,22 @@ let self = {
 
     log(opts, `extracted all files ${archive_path} into staging area`)
 
-    let stage_files = await sf.glob('**', {cwd: stage_path})
-
-    if (stage_files.length === 1) {
-      let only_file = path.join(stage_path, stage_files[0])
-
-      if (!opts.tar && await is_tar(only_file)) {
-        // Files in .tar.gz, .tar.bz2, etc. need a second 7-zip invocation
-        let tar = only_file
-        log(opts, `found tar: ${tar}, re-extracting`)
-        let sub_opts = Object.assign({}, opts, {
-          archive_path: tar,
-          tar: true,
-          onprogress: stagecp_onprogress
-        })
-
-        let res = await self.install(sub_opts)
-        await butler.wipe(tar)
-        return res
-      } else {
-        // zipped installers need love too
-        let sniff_opts = {archive_path: only_file, disable_cache: true}
-        let installer_name
-
-        try {
-          installer_name = await core.sniff_type(sniff_opts)
-        } catch (err) {
-          log(opts, `only file isn't a recognized installer type`)
-          installer_name = null
-        }
-
-        if (installer_name) {
-          onprogress({percent: 0})
-          self.cache_type(opts, installer_name)
-          log(opts, `found nested installer '${installer_name}', going with it!`)
-          let nested_opts = Object.assign({}, opts, sniff_opts, {
-            onprogress: stagecp_onprogress
-          })
-          log(opts, `giving nested_opts: ${JSON.stringify(nested_opts, null, 2)}`)
-          return await core.install(nested_opts)
-        }
-      }
-    }
-
-    await butler.mkdir(dest_path)
-
-    log(opts, `cleaning up dest path ${dest_path}`)
-
-    // XXX write stage file list when all is said and done / read it back when upgrading
-    let receipt_path = path.join(dest_path, '.itch', 'receipt.json')
-    let dest_files = []
-
-    try {
-      let receipt_contents = await sf.read_file(receipt_path)
-      let receipt = JSON.parse(receipt_contents)
-      dest_files = receipt.files || []
-      log(opts, `Got receipt for an existing ${dest_files.length}-files install.`)
-    } catch (err) {
-      log(opts, `Could not read receipt: ${err.message}`)
-    }
-    if (!dest_files.length) {
-      log(opts, `Globbing for destfiles`)
-      dest_files = await sf.glob('**', {cwd: dest_path})
-    }
-
-    log(opts, `dest has ${dest_files.length} potential dinosaurs`)
-
-    let dinosaurs = _.difference(dest_files, stage_files)
-    if (dinosaurs.length) {
-      log(opts, `removing ${dinosaurs.length} dinosaurs in dest`)
-      log(opts, `example dinosaurs: ${JSON.stringify(dinosaurs.slice(0, 10), null, 2)}`)
-
-      await Promise.resolve(dinosaurs).map((rel) => {
-        let dinosaur = path.join(dest_path, rel)
-        return butler.wipe(dinosaur)
-      }, {concurrency: 4})
-    } else {
-      log(opts, `no dinosaurs`)
-    }
-
-    log(opts, `copying stage to dest`)
-    await butler.ditto(stage_path, dest_path, {
-      onprogress: stagecp_onprogress,
-      emitter: emitter
+    let deploy_opts = Object.assign({}, opts, {
+      onprogress: deploy_onprogress,
+      stage_path
     })
 
-    log(opts, `everything copied, writing receipt`)
-    let cave = opts.cave || {}
+    deploy_opts.onsingle = async (only_file) => {
+      if (!opts.tar && await is_tar(only_file)) {
+        return await self.handle_tar(deploy_opts, only_file)
+      }
 
-    await sf.write_file(receipt_path, JSON.stringify({
-      cave,
-      num_files: stage_files.length,
-      files: stage_files
-    }, null, 2))
+      return await self.handle_nested(deploy_opts, only_file)
+    }
+
+    await deploy.deploy(deploy_opts)
+
+    await butler.mkdir(dest_path)
 
     log(opts, `wiping stage...`)
     await butler.wipe(stage_path)
@@ -187,6 +105,38 @@ let self = {
 
     log(opts, `cleaning up cache`)
     self.cache_type(opts, null)
+  },
+
+  handle_tar: async function (opts, tar) {
+    // Files in .tar.gz, .tar.bz2, etc. need a second 7-zip invocation
+    log(opts, `extracting tar: ${tar}`)
+    let sub_opts = Object.assign({}, opts, {
+      archive_path: tar,
+      tar: true
+    })
+
+    let res = await self.install(sub_opts)
+    await butler.wipe(tar)
+    return res
+  },
+
+  handle_nested: async function (opts, only_file) {
+    // zipped installers need love too
+    let sniff_opts = {archive_path: only_file, disable_cache: true}
+    let installer_name
+
+    try {
+      installer_name = await core.sniff_type(sniff_opts)
+    } catch (err) {
+      log(opts, `not a recognized installer type: ${only_file}`)
+      return null
+    }
+
+    self.cache_type(opts, installer_name)
+    log(opts, `found a '${installer_name}': ${only_file}`)
+    let nested_opts = Object.assign({}, opts, sniff_opts)
+    log(opts, `installing it with nested_opts: ${JSON.stringify(nested_opts, null, 2)}`)
+    return await core.install(nested_opts)
   }
 }
 
---
+++ app/util/deploy.js
@@ -0,0 +1,95 @@
+
+let sf = require('./sf')
+let noop = require('./noop')
+let butler = require('./butler')
+
+let _ = require('underscore')
+let path = require('path')
+
+let log = require('./log')('staged-extract')
+
+let pnoop = async () => null
+
+let self = {
+  /**
+   * Given a stage_path, and a dest_path
+   *   - Figures out which files disappeared from stage since last deploying to dest
+   *   - Removes those
+   *   - Copy all the new files from stage to dest, overwriting
+   *   - Write receipt with list of files present in stage at deploy time
+   *     (that receipt will be used on next deploy)
+   */
+  deploy: async (opts) => {
+    let stage_path = opts.stage_path
+    let dest_path = opts.dest_path
+    let emitter = opts.emitter
+
+    let onprogress = opts.onprogress || noop
+    let onsingle = opts.onsingle || pnoop
+
+    let stage_files = await sf.glob('**', {cwd: stage_path})
+
+    if (stage_files.length === 1) {
+      let only_file = path.join(stage_path, stage_files[0])
+      let res = await onsingle(only_file)
+      if (res) {
+        // onsingle returning true means it's been handled upstraem
+        return res
+      }
+    }
+
+    await butler.mkdir(dest_path)
+
+    log(opts, `cleaning up dest path ${dest_path}`)
+
+    let receipt_path = path.join(dest_path, '.itch', 'receipt.json')
+    let dest_files = []
+
+    try {
+      let receipt_contents = await sf.read_file(receipt_path)
+      let receipt = JSON.parse(receipt_contents)
+      dest_files = receipt.files || []
+      log(opts, `Got receipt for an existing ${dest_files.length}-files install.`)
+    } catch (err) {
+      log(opts, `Could not read receipt: ${err.message}`)
+    }
+    if (!dest_files.length) {
+      log(opts, `Globbing for destfiles`)
+      dest_files = await sf.glob('**', {cwd: dest_path})
+    }
+
+    log(opts, `dest has ${dest_files.length} potential dinosaurs`)
+
+    let dinosaurs = _.difference(dest_files, stage_files)
+    if (dinosaurs.length) {
+      log(opts, `removing ${dinosaurs.length} dinosaurs in dest`)
+      log(opts, `example dinosaurs: ${JSON.stringify(dinosaurs.slice(0, 10), null, 2)}`)
+
+      await Promise.resolve(dinosaurs).map((rel) => {
+        let dinosaur = path.join(dest_path, rel)
+        return butler.wipe(dinosaur)
+      }, {concurrency: 4})
+    } else {
+      log(opts, `no dinosaurs`)
+    }
+
+    log(opts, `copying stage to dest`)
+    await butler.ditto(stage_path, dest_path, {
+      onprogress,
+      emitter: emitter
+    })
+
+    log(opts, `everything copied, writing receipt`)
+    let cave = opts.cave || {}
+
+    await sf.write_file(receipt_path, JSON.stringify({
+      cave,
+      num_files: stage_files.length,
+      files: stage_files
+    }, null, 2))
+
+    return {status: 'ok'}
+  }
+}
+
+module.exports = self
