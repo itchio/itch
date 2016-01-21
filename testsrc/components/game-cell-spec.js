0d800aff3565f059a15ebf0089d86e19722d906c
Amos Wenger <fasterthanlime@gmail.com>
2016-01-21 18:19:38 +0100

    :art: Split game-list & game-cell specs

---
+++ testsrc/components/game-cell-spec.js
@@ -0,0 +1,32 @@
+
+let test = require('zopf')
+let mori = require('mori')
+let proxyquire = require('proxyquire')
+let _ = require('underscore')
+
+let sd = require('./skin-deeper')
+let stubs = require('../stubs/react-stubs')
+
+test('GameCell', t => {
+  let GameCell = proxyquire('../../app/components/game-cell', stubs)
+  let game = mori.toClj({
+    title: 'a',
+    cover_url: 'b',
+    user: {
+      display_name: 'd'
+    },
+    p_android: true,
+    p_windows: true,
+    p_linux: true,
+    p_osx: true
+  })
+  let install = null
+
+  sd.shallowRender(sd(GameCell, {game, install}))
+  install = mori.assoc(install, 'progress', 0.2)
+
+  ;['download', 'extract', 'idle', 'error', 'launch'].forEach((task) => {
+    install = mori.assoc(install, 'task', task)
+    sd.shallowRender(sd(GameCell, {game, install}))
+  })
+})
--- testsrc/components/game-list-spec.js
+++ testsrc/components/game-list-spec.js
@@ -7,38 +7,12 @@ let _ = require('underscore')
 let sd = require('./skin-deeper')
 let stubs = require('../stubs/react-stubs')
 
-test('game-list', t => {
-  t.case('GameList', t => {
-    let GameList = proxyquire('../../app/components/game-list', stubs)
-    sd.shallowRender(sd(GameList, {}))
-    let games = mori.toClj(_.indexBy([{id: 12}, {id: 26}, {id: 42}], 'id'))
-    let installs = mori.toClj({
-      'asd09f8': {game_id: 42}
-    })
-    sd.shallowRender(sd(GameList, {games, installs}))
+test('GameList', t => {
+  let GameList = proxyquire('../../app/components/game-list', stubs)
+  sd.shallowRender(sd(GameList, {}))
+  let games = mori.toClj(_.indexBy([{id: 12}, {id: 26}, {id: 42}], 'id'))
+  let installs = mori.toClj({
+    'asd09f8': {game_id: 42}
   })
-
-  t.case('GameCell', t => {
-    let GameCell = proxyquire('../../app/components/game-cell', stubs)
-    let game = mori.toClj({
-      title: 'a',
-      cover_url: 'b',
-      user: {
-        display_name: 'd'
-      },
-      p_android: true,
-      p_windows: true,
-      p_linux: true,
-      p_osx: true
-    })
-    let install = null
-
-    sd.shallowRender(sd(GameCell, {game, install}))
-    install = mori.assoc(install, 'progress', 0.2)
-
-    ;['download', 'extract', 'idle', 'error', 'launch'].forEach((task) => {
-      install = mori.assoc(install, 'task', task)
-      sd.shallowRender(sd(GameCell, {game, install}))
-    })
-  })
+  sd.shallowRender(sd(GameList, {games, installs}))
 })
