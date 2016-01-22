1eb5d2e478f933ec91bda998f1aae020ee358eba
Amos Wenger <fasterthanlime@gmail.com>
2016-01-22 18:28:08 +0100

    :art: Use mori-ext in GameList component #412

--- appsrc/components/game-list.js
+++ appsrc/components/game-list.js
@@ -1,6 +1,7 @@
 
+import {zipmap, hasKey, get, vals, each, map} from 'mori-ext'
+
 let r = require('r-dom')
-let mori = require('mori')
 let PropTypes = require('react').PropTypes
 let ShallowComponent = require('./shallow-component')
 
@@ -14,29 +15,25 @@ let always_true = () => true
  */
 class GameList extends ShallowComponent {
   render () {
+    let {games, caves, owned_games_by_id, is_press} = this.props
     let pred = this.props.pred || always_true
-    let games = this.props.games
-    let caves = this.props.caves
-    let owned_games_by_id = this.props.owned_games_by_id
-    let is_press = this.props.is_press
 
-    let index_by = (acc, k, v) => mori.assoc(acc, mori.get(v, 'game_id'), v)
     // TODO perf: app-store should maintain this instead of us recomputing it
     // every time GameList is dirty
-    let caves_by_game_id = mori.reduceKV(index_by, mori.hashMap(), caves)
+    let caves_by_game_id = ((x) => x::get('game_id'))::map(caves::vals())::zipmap(caves::vals())
 
     let children = []
 
     // TODO perf: game-list should only filter & pass down immutable
     // substructures, and let owned computation to children so that we
     // take advantage of dirty checking
-    mori.each(mori.vals(games), (game) => {
-      let game_id = mori.get(game, 'id')
-      let cave = mori.get(caves_by_game_id, game_id)
+    games::vals()::each((game) => {
+      let game_id = game::get('id')
+      let cave = caves_by_game_id::get(game_id)
       if (!pred(cave)) return
 
-      let owned_via_library = mori.hasKey(owned_games_by_id, game_id.toString())
-      let owned_via_press_system = !!(is_press && mori.get(game, 'in_press_system'))
+      let owned_via_library = owned_games_by_id::hasKey(game_id.toString())
+      let owned_via_press_system = !!(is_press && game::get('in_press_system'))
       let owned = owned_via_library || owned_via_press_system
 
       children.push(
