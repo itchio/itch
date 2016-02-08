568607fe29b91878d6db782ecac92f66c6dd5faf
Amos Wenger <fasterthanlime@gmail.com>
2016-02-08 21:09:56 +0100

    Strip Electron from user-agent for the bad agent sniffers out there! Don't want to point fingers @elisee

--- appsrc/tasks/launch/html.js
+++ appsrc/tasks/launch/html.js
@@ -15,19 +15,33 @@ let self = {
   launch: async function(opts, cave) {
     let game = await db.find_one({_table: 'games', id: cave.game_id})
     let app_path = path.join(CaveStore.app_path(cave.install_location, opts.id), cave.game_path)
+
     log(opts, `game root: ${app_path}`)
     let win = new BrowserWindow({
       title: game.title,
       icon: './static/images/itchio-tray-x4.png',
-      width: cave.window_size.width, height: cave.window_size.height,
+      width: cave.window_size.width,
+      height: cave.window_size.height,
       center: true,
       show: true,
       autoHideMenuBar: true,
       webPreferences: {
+        /* don't let web code control the OS */
         nodeIntegration: false,
+        /* stores cookies etc. in persistent session to save progress */
         partition: `persist:gamesession_${cave.game_id}`
       }
     })
+
+    // strip 'Electron' from user agent so some web games stop being confused
+    let userAgent = win.webContents.getUserAgent()
+    userAgent = userAgent.replace(/Electron\/[0-9.]+\s/, '')
+    win.webContents.setUserAgent(userAgent)
+
+    if (process.env.IMMEDIATE_NOSE_DIVE === '1') {
+      win.webContents.openDevTools({detach: true})
+    }
+
     let serve = serveStatic(path.dirname(app_path), {'index': [path.basename(app_path)]})
     let server = http.createServer((req, res) => {
       let done = finalhandler(req, res)
@@ -35,6 +49,7 @@ let self = {
     })
     let port
     server.listen(0) // let node/os assign random port
+
     server.on('listening', function () {
       port = server.address().port
       log(opts, `serving game on port ${port}`)
