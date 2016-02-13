835612ff7f2e93a412b62eb10e8a5c284402fe33
Andy <andynoelker@gmail.com>
2016-02-12 23:04:05 -0600

    :white_check_mark: Add preferences form spec

---
+++ testsrc/components/preferences-form-spec.js
@@ -0,0 +1,31 @@
+
+let test = require('zopf')
+let mori = require('mori')
+let proxyquire = require('proxyquire')
+
+let sd = require('./skin-deeper')
+let stubs = require('../stubs/react-stubs')
+
+test('PreferencesForm', t => {
+  let PreferencesForm = proxyquire('../../app/components/preferences-form', stubs)
+  let appdata = mori.toClj({
+    name: 'appdata',
+    size: -1,
+    free_space: 202006237184,
+    item_count: 0,
+    computing_size: false,
+    path: ''
+  })
+  let state = mori.toClj({
+    'install-locations': {
+      aliases: [
+        ["","~"]
+      ],
+      default: 'appdata',
+      locations: { appdata }
+    }
+  })
+
+  let tree = sd.shallowRender(sd(PreferencesForm, { state }))
+  let instance = tree.getMountedInstance()
+})
