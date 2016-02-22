0e301a39272eac6984f68d08c072af2e5d5ee074
Amos Wenger <fasterthanlime@gmail.com>
2016-02-22 18:10:21 +0100

    :green_heart: configure spec

--- testsrc/stubs/cave-store.js
+++ testsrc/stubs/cave-store.js
@@ -3,6 +3,7 @@ module.exports = {
   find: async () => ({
     _id: 'kalamazoo',
     upload_id: 42,
+    uploads: [ {id: 42, filename: 'hehe'} ],
     game_id: 84,
     launch_type: 'native'
   }),
