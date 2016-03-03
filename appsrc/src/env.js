911fe3761f4f6e3af2fb74cd289570281af6716b
Amos Wenger <fasterthanlime@gmail.com>
2016-03-03 16:47:21 +0100

    make 'npm run local-cov' faster (and work)

---
+++ appsrc/src/env.js
@@ -0,0 +1,7 @@
+
+// this file has no real purpose except than to make
+// coverage reports work... it really insists that there's something in there,
+// although that file didn't exist before I (amos) added it so that it
+// would keep generating our HTML coverage report.
+
+// tl;dr instead of `app/src/env.js`, you're probably looking for `appsrc/env.js`
