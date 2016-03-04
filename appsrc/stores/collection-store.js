81471a61b3a6dc74654009c25f4f57b7ceb13051
Amos Wenger <fasterthanlime@gmail.com>
2016-03-04 21:12:06 +0100

    Add a featured collection

--- appsrc/stores/collection-store.js
+++ appsrc/stores/collection-store.js
@@ -17,6 +17,7 @@ const state = {
     // 37741, // Early 2016 picks
     32705, // Bite-sized gems
     22615,  // SirTapTap's cool weird games
+    2925, // cool weirdgame art shit for you
     11, // Leaf's cool stuff
     46704 // Tools & stuff
   ]
