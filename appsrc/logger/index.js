710a4452230d74b14d1a76b65794844e80b6164c
Amos Wenger <fasterthanlime@gmail.com>
2016-03-12 21:57:13 +0100

    Print electron version on boot

--- appsrc/logger/index.js
+++ appsrc/logger/index.js
@@ -2,3 +2,7 @@
 import mklog from '../util/log'
 export const logger = new mklog.Logger()
 export default logger
+
+const log = mklog('itch')
+const opts = {logger}
+log(opts, `using electron ${process.versions.electron}`)
