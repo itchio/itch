8f984c05ded92f5c7bf58cb67c3ec40f8e14ada0
Amos Wenger <fasterthanlime@gmail.com>
2016-03-23 19:30:37 +0100

    :trophy: Use CI_BUILD_REF_NAME to publish book

--- release/ci-book.sh
+++ release/ci-book.sh
@@ -5,9 +5,4 @@ npm install -g gitbook-cli
 
 (cd docs && gitbook build)
 
-CI_VERSION="head"
-if [ -n "$CI_BUILD_TAG" ]; then
-  CI_VERSION="$CI_BUILD_TAG"
-fi
-
-gsutil cp -r -a public-read docs/_book/* gs://docs.itch.ovh/itch/$CI_VERSION/
+gsutil cp -r -a public-read docs/_book/* gs://docs.itch.ovh/itch/$CI_BUILD_REF_NAME/
