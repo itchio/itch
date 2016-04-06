8828ef24f41a3b732d7946b88e9455a79fda5f51
Amos Wenger <fasterthanlime@gmail.com>
2016-04-06 15:51:31 +0200

    Compute cave size at configure time

--- appsrc/tasks/configure.js
+++ appsrc/tasks/configure.js
@@ -1,6 +1,8 @@
 
 import invariant from 'invariant'
+import humanize from 'humanize-plus'
 
+import walk from 'walk'
 import os from '../util/os'
 
 import mklog from '../util/log'
@@ -8,6 +10,7 @@ const log = mklog('tasks/configure')
 import pathmaker from '../util/pathmaker'
 
 import html from './configure/html'
+import {each} from 'underline'
 
 async function configure (appPath) {
   const platform = os.platform()
@@ -44,4 +47,27 @@ export default async function start (out, opts) {
     log(opts, `native-configure yielded execs: ${JSON.stringify(executables, null, 2)}`)
     globalMarket.saveEntity('caves', cave.id, {executables})
   }
+
+  log(opts, `computing size of ${appPath}`)
+  const walker = walk.walk(appPath, {followLinks: false})
+
+  let totalSize = 0
+  walker.on('file', (root, fileStats, next) => {
+    totalSize += fileStats.size
+    next()
+  })
+
+  walker.on('errors', (root, nodeStatsArray, next) => {
+    nodeStatsArray::each((n) => {
+      log(opts, `error while walking ${n.name}:`)
+      log(opts, n.error.message || (n.error.code + ': ' + n.error.path))
+    })
+    next()
+  })
+
+  await new Promise((resolve, reject) => {
+    walker.on('end', resolve)
+  })
+  log(opts, `total size of ${appPath}: ${humanize.fileSize(totalSize)} (${totalSize} bytes)`)
+  globalMarket.saveEntity('caves', cave.id, {installedSize: totalSize})
 }
