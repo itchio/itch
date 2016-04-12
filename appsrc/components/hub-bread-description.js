c2cba4800d77f6369b53a0885eaab01ca4fa613c
Amos Wenger <fasterthanlime@gmail.com>
2016-04-12 15:24:17 +0200

    Too many icons kill the icon

--- appsrc/components/hub-bread-description.js
+++ appsrc/components/hub-bread-description.js
@@ -2,15 +2,13 @@
 import React, {Component, PropTypes} from 'react'
 import {connect} from './connect'
 
-import {pathToIcon} from '../util/navigation'
-
 class HubBreadDescription extends Component {
   render () {
     const {t, path, tabData} = this.props
-    const {icon = pathToIcon(path), label = 'Loading...', subtitle} = tabData[path] || {}
+    const {label = 'Loading...', subtitle} = tabData[path] || {}
 
     return <section className='description'>
-      <h2><icon className={`icon icon-${icon}`}/> {t.format(label)}</h2>
+      <h2>{t.format(label)}</h2>
       <h3>{t.format(subtitle)}</h3>
     </section>
   }
