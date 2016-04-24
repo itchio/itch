bf5f1ee73ab995953aaf0575e113cb563f02e4ae
Amos Wenger <fasterthanlime@gmail.com>
2016-04-24 20:33:08 +0200

    If incremental upgrades fail, do full download

--- appsrc/sagas/tasks/download-ended.js
+++ appsrc/sagas/tasks/download-ended.js
@@ -3,17 +3,33 @@ import {call} from 'redux-saga/effects'
 import invariant from 'invariant'
 
 import {startTask} from './start-task'
+import {startDownload} from './start-download'
 import {log, opts} from './log'
+
+import {omit} from 'underline'
 
 export function * _downloadEnded (action) {
   const {downloadOpts} = action.payload
   let {err} = action.payload
 
   const {reason, incremental} = downloadOpts
-  if ((reason === 'install' || reason === 'update') && !incremental) {
+  if (reason === 'install' || reason === 'update') {
     if (err) {
-      log(opts, 'Download had an error, should notify user')
+      if (incremental) {
+        log(opts, `Incremental didn't work, doing full download`)
+        const newDownloadOpts = {
+          ...downloadOpts::omit('upgradePath', 'incremental'),
+          totalSize: downloadOpts.upload.size
+        }
+        yield call(startDownload, newDownloadOpts)
+      } else {
+        log(opts, 'Download had an error, should notify user')
+      }
     } else {
+      if (incremental) {
+        // all good
+        return
+      }
       log(opts, 'Download finished, installing..')
 
       const {gameId} = downloadOpts
