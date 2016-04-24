4d43405fb585b634e7381ec37ac59df1b9601a6e
Amos Wenger <fasterthanlime@gmail.com>
2016-04-24 16:40:13 +0200

    Decouple secondary actions list making

---
+++ appsrc/components/game-actions/list-secondary-actions.js
@@ -0,0 +1,99 @@
+
+import invariant from 'invariant'
+
+import os from '../../util/os'
+const platform = os.itchPlatform()
+
+import * as actions from '../../actions'
+
+function browseI18nKey () {
+  let fallback = 'grid.item.open_in_file_explorer'
+  switch (platform) {
+    case 'osx': return ['grid.item.open_in_file_explorer_osx', fallback]
+    case 'linux': return ['grid.item.open_in_file_explorer_linux', fallback]
+    default: return fallback
+  }
+}
+
+function browseAction (caveId) {
+  return {
+    label: [browseI18nKey()],
+    icon: 'folder-open',
+    action: actions.exploreCave({caveId})
+  }
+}
+
+function purchaseAction (game) {
+  invariant(typeof game === 'object', 'game is object')
+  return {
+    label: ['grid.item.purchase_or_donate'],
+    icon: 'cart',
+    action: actions.initiatePurchase({game})
+  }
+}
+
+function retryAction (gameId) {
+  return {
+    label: ['grid.item.retry'],
+    icon: 'repeat',
+    action: actions.queueGame({gameId})
+  }
+}
+
+function probeAction (caveId) {
+  return {
+    label: ['grid.item.probe'],
+    icon: 'bug',
+    action: actions.probeCave({caveId})
+  }
+}
+
+function uninstallAction (caveId) {
+  return {
+    label: ['grid.item.uninstall'],
+    icon: 'delete',
+    action: actions.requestCaveUninstall({caveId})
+  }
+}
+
+export default function listSecondaryActions (props) {
+  const {task, game, cave, mayDownload, action} = props
+  let error = false
+
+  const items = []
+
+  if (cave) {
+    if (task === 'error') {
+      error = true
+
+      items.push(retryAction(game.id))
+      items.push(browseAction(cave.id))
+      items.push(probeAction(cave.id))
+    }
+
+    if (task === 'idle') {
+      // No errors
+      items.push(purchaseAction(game))
+
+      if (action !== 'open') {
+        items.push(browseAction(cave.id))
+      }
+    }
+
+    if (task === 'error' || task === 'idle') {
+      items.push(uninstallAction(cave.id))
+    }
+  } else {
+    // No cave
+    const hasMinPrice = game.minPrice > 0
+    const mainIsPurchase = !mayDownload && hasMinPrice
+
+    // XXX should use API' can_be_bought but see
+    // https://github.com/itchio/itch/issues/379
+    if (!mainIsPurchase) {
+      items.push(purchaseAction(game))
+    }
+  }
+
+  return {error, items}
+}
--- appsrc/components/game-actions/secondary-actions.js
+++ appsrc/components/game-actions/secondary-actions.js
@@ -1,116 +1,29 @@
 
 import React, {Component, PropTypes} from 'react'
-import invariant from 'invariant'
 import classNames from 'classnames'
 
 import {connect} from '../connect'
 import Icon from '../icon'
 
-import os from '../../util/os'
-const platform = os.itchPlatform()
+import listSecondaryActions from './list-secondary-actions'
+import {map} from 'underline'
 
 class SecondaryActions extends Component {
   render () {
-    const {task, game, cave, mayDownload, action} = this.props
-    let error = false
-
-    const children = []
-
-    if (cave) {
-      if (task === 'error') {
-        error = true
-
-        children.push(this.retryAction(game.id))
-        children.push(this.browseAction(cave.id))
-        children.push(this.probeAction(cave.id))
-      }
-
-      if (task === 'idle') {
-        // No errors
-        children.push(this.purchaseAction(game))
-
-        if (action !== 'open') {
-          children.push(this.browseAction(cave.id))
-        }
-      }
-
-      if (task === 'error' || task === 'idle') {
-        children.push(this.uninstallAction(cave.id))
-      }
-    } else {
-      // No cave
-      const hasMinPrice = game.minPrice > 0
-      const mainIsPurchase = !mayDownload && hasMinPrice
-
-      // XXX should use API' can_be_bought but see
-      // https://github.com/itchio/itch/issues/379
-      if (!mainIsPurchase) {
-        children.push(this.purchaseAction(game))
-      }
-    }
+    const {items, error} = listSecondaryActions(this.props)
 
     return <div className={classNames('cave-actions', {error})}>
-      {children}
+      { items::map(::this.action) }
     </div>
   }
 
   action (opts) {
-    const {t} = this.props
-    const {label, icon, onClick} = opts
+    const {t, dispatch} = this.props
+    const {action, label, icon} = opts
 
-    return <span key={label} className='secondary-action hint--top' onClick={onClick} data-hint={t.format(label)}>
+    return <span key={label} className='secondary-action hint--top' onClick={() => dispatch(action)} data-hint={t.format(label)}>
       <Icon icon={icon}/>
     </span>
-  }
-
-  browseI18nKey () {
-    let fallback = 'grid.item.open_in_file_explorer'
-    switch (platform) {
-      case 'osx': return ['grid.item.open_in_file_explorer_osx', fallback]
-      case 'linux': return ['grid.item.open_in_file_explorer_linux', fallback]
-      default: return fallback
-    }
-  }
-
-  browseAction (caveId) {
-    return this.action({
-      label: [this.browseI18nKey()],
-      icon: 'folder-open',
-      onClick: () => this.props.exploreCave(caveId)
-    })
-  }
-
-  purchaseAction (game) {
-    invariant(typeof game === 'object', 'game is object')
-    return this.action({
-      label: ['grid.item.purchase_or_donate'],
-      icon: 'cart',
-      onClick: () => this.props.initiatePurchase(game)
-    })
-  }
-
-  retryAction (gameId) {
-    return this.action({
-      label: ['grid.item.retry'],
-      icon: 'repeat',
-      onClick: () => this.props.queueGame(gameId)
-    })
-  }
-
-  probeAction (caveId) {
-    return this.action({
-      label: ['grid.item.probe'],
-      icon: 'bug',
-      onClick: () => this.props.probeCave(caveId)
-    })
-  }
-
-  uninstallAction (caveId) {
-    return this.action({
-      label: ['grid.item.uninstall'],
-      icon: 'delete',
-      onClick: () => this.props.requestCaveUninstall(caveId)
-    })
   }
 }
 
@@ -122,11 +35,13 @@ SecondaryActions.propTypes = {
   action: PropTypes.string,
 
   t: PropTypes.func.isRequired,
-  probeCave: PropTypes.func.isRequired,
-  queueGame: PropTypes.func.isRequired,
-  initiatePurchase: PropTypes.func.isRequired,
-  exploreCave: PropTypes.func.isRequired,
-  requestCaveUninstall: PropTypes.func.isRequired
+  dispatch: PropTypes.func.isRequired
 }
 
-export default connect()(SecondaryActions)
+const mapStateToProps = (state) => ({})
+const mapDispatchToProps = (dispatch) => ({dispatch})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SecondaryActions)
