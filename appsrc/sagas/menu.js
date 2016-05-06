83c7b2f6b0e27082c092a329fdd7d7c305c57d75
Amos Wenger <fasterthanlime@gmail.com>
2016-05-07 01:19:27 +0200

    :bug: Fix menu links

--- appsrc/sagas/menu.js
+++ appsrc/sagas/menu.js
@@ -60,7 +60,7 @@ import {
   refreshMenu,
   menuAction,
   navigate,
-  openExternal,
+  openUrl,
   changeUser,
   checkForSelfUpdate,
   reportIssue,
@@ -82,12 +82,12 @@ function convertMenuAction (label) {
     case 'menu.view.downloads': return navigate('downloads')
     case 'menu.view.history': return navigate('history')
     case 'menu.account.change_user': return changeUser()
-    case 'menu.help.view_terms': return openExternal(urls.termsOfService)
-    case 'menu.help.view_license': return openExternal(`${urls.itchRepo}/blob/master/LICENSE`)
+    case 'menu.help.view_terms': return openUrl(urls.termsOfService)
+    case 'menu.help.view_license': return openUrl(`${urls.itchRepo}/blob/master/LICENSE`)
     case 'menu.help.check_for_update': return checkForSelfUpdate()
     case 'menu.help.report_issue': return reportIssue()
-    case 'menu.help.search_issue': return openExternal(`${urls.itchRepo}/search?type=Issues`)
-    case 'menu.help.release_notes': return openExternal(`${urls.itchRepo}/releases`)
+    case 'menu.help.search_issue': return openUrl(`${urls.itchRepo}/search?type=Issues`)
+    case 'menu.help.release_notes': return openUrl(`${urls.itchRepo}/releases`)
     default: console.log(`Unhandled menu action: ${label}`)
   }
 }
