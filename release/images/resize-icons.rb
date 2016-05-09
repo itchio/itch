4676d764af88611cb9c21b96710c47c6eae92c88
Amos Wenger <fasterthanlime@gmail.com>
2016-05-09 22:02:20 +0200

    icon update

---
+++ release/images/resize-icons.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+Dir["*-icons/"].each do |dir|
+  %w(16 32 36 48 64 72 114 128 144 150 256 512 1024).each do |size|
+    puts "#{dir}#{size}"
+    system("convert #{dir}source.png -resize #{size}x#{size} #{dir}icon#{size}.png")
+  end
+end
