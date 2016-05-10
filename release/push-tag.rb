4e95b40acbb6f087c00790cb7dd2b61a43d09d0c
Amos Wenger <fasterthanlime@gmail.com>
2016-05-10 15:20:37 +0200

    CD: Improve push-tag script

--- release/push-tag.rb
+++ release/push-tag.rb
@@ -2,6 +2,8 @@
 
 require_relative 'common'
 
+force = false
+
 def prompt (msg)
   print "#{msg}: "
   $stdout.flush
@@ -11,6 +13,12 @@ end
 def yesno (msg)
   print "#{msg} (y/n) "
   $stdout.flush
+
+  if force
+    puts " (forced yes)"
+    return
+  end
+
   unless "y" == gets.strip
     print "Bailing out..."
     exit 0
@@ -23,7 +31,20 @@ pkg_path = File.expand_path(File.join(
 pkg = JSON.parse(File.read(pkg_path))
 pkg_version = pkg['version']
 
-version_input = prompt "Package version is: #{pkg['version']}, type yours"
+args = []
+ARGV.each do |arg|
+  case arg
+  when "--force"
+    puts "(Running in forced mode)"
+    force = true
+  when /^--/
+    raise "Unknown option #{arg}"
+  else
+    args << arg
+  end
+end
+
+version_input = args[0] or prompt "Package version is: #{pkg['version']}, type yours"
 unless /^v\d+.\d+.\d+(-canary)?$/ =~ version_input
   raise "Version must be of the form vX.Y.Z(-canary)?"
 end
@@ -34,7 +55,7 @@ if pkg_version != next_version
   yesno "Bump package.json? [#{pkg_version} => #{next_version}]"
   pkg['version'] = next_version
   File.write(pkg_path, JSON.pretty_generate(pkg))
-  say "Bumped package.json"
+  puts "Bumped package.json"
   system %Q{git add package.json} or raise
   system %Q{git commit -m ':arrow_up: #{next_version}'} or raise
 end
@@ -43,7 +64,7 @@ tag = "v#{next_version}"
 add_cmd = %Q{git tag -a #{tag} -m #{tag}}
 
 if system add_cmd
-  say "Tag added..."
+  puts "Tag added..."
 else
   yesno "Tag already exists locally. Replace?"
   system %Q{git tag -d #{tag}} or raise
@@ -52,7 +73,7 @@ end
 
 push_cmd = %Q{git push origin #{tag}}
 if system push_cmd
-  say "Tag pushed..."
+  puts "Tag pushed..."
 else
   yesno "Tag already exists on remote. Force-push?"
   system %Q{#{push_cmd} --force} or raise
