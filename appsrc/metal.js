a27ba3a669b14e7d856f868b40b61d3e33796aab
Amos Wenger <fasterthanlime@gmail.com>
2016-05-13 11:21:03 +0200

    Attempt to handle Squirrel handlers faster, cf. #635

--- appsrc/metal.js
+++ appsrc/metal.js
@@ -12,94 +12,10 @@ import './boot/fs'
 import autoUpdater from './util/auto-updater'
 import {isItchioURL} from './util/url'
 
-async function autoUpdate () {
-  const quit = await autoUpdater.start()
-  if (quit) {
-    // squirrel on win32 sometimes requires exiting as early as possible
-    process.exit(0)
-  }
-}
-
-autoUpdate() // no need to wait for app.on('ready')
-
-// App lifecycle
-
-import {app} from './electron'
-
-import {
-  preboot,
-  prepareQuit,
-  focusWindow,
-  openUrl
-} from './actions'
-import store from './store'
-
-app.on('ready', async () => {
-  const shouldQuit = app.makeSingleInstance((argv, cwd) => {
-    // we only get inside this callback when another instance
-    // is launched - so this executes in the context of the main instance
-    handleUrls(argv)
-    store.dispatch(focusWindow())
-  })
-
-  if (shouldQuit) {
-    // app.quit() is the source of all our problems,
-    // cf. https://github.com/itchio/itch/issues/202
-    process.exit(0)
-    return
-  }
-  handleUrls(process.argv)
-
-  store.dispatch(preboot())
-})
-
-app.on('activate', () => {
-  store.dispatch(focusWindow())
-})
-
-app.on('before-quit', (e) => {
-  store.dispatch(prepareQuit())
-})
-
-app.on('window-all-closed', (e) => {
-  const state = store.getState()
-  if (state.ui.mainWindow.quitting) {
-    // let normal electron shutdown process continue
-    return
-  } else {
-    // prevent electron shutdown, we want to remain alive
-    e.preventDefault()
-  }
-})
-
-// OSX (Info.pList)
-app.on('open-url', (e, url) => {
-  if (isItchioURL(url)) {
-    // OSX will err -600 if we don't
-    e.preventDefault()
-    store.dispatch(openUrl(url))
-  } else {
-    console.log(`Ignoring non-itchio url: ${url}`)
-  }
-})
-
-// URL handling
-
-function handleUrls (argv) {
-  // Windows (reg.exe), Linux (XDG)
-  argv.forEach((arg) => {
-    // XXX should we limit to one url at most ?
-    if (isItchioURL(arg)) {
-      store.dispatch(openUrl(arg))
-    }
-  })
-}
-
 // Fresh installs
-
-import fs from 'fs'
-
 (function () {
+  const fs = require('fs')
+  const {app} = require('electron')
   const userDataPath = app.getPath('userData')
   console.log(`Creating ${userDataPath} in case it doesn't exist..`)
 
@@ -113,3 +29,90 @@ import fs from 'fs'
     console.log(`While creating ${userDataPath}: ${err.stack || err}`)
   }
 })()
+
+async function autoUpdate (autoUpdateDone) {
+  const quit = await autoUpdater.start()
+  if (quit) {
+    // squirrel on win32 sometimes requires exiting as early as possible
+    process.exit(0)
+  } else {
+    autoUpdateDone()
+  }
+}
+
+autoUpdate(autoUpdateDone) // no need to wait for app.on('ready')
+
+// App lifecycle
+
+function autoUpdateDone () {
+  const app = require('electron').app
+
+  const {
+    preboot,
+    prepareQuit,
+    focusWindow,
+    openUrl
+  } = require('./actions')
+  const store = require('./store').default
+
+  app.on('ready', async () => {
+    const shouldQuit = app.makeSingleInstance((argv, cwd) => {
+      // we only get inside this callback when another instance
+      // is launched - so this executes in the context of the main instance
+      handleUrls(argv)
+      store.dispatch(focusWindow())
+    })
+
+    if (shouldQuit) {
+      // app.quit() is the source of all our problems,
+      // cf. https://github.com/itchio/itch/issues/202
+      process.exit(0)
+      return
+    }
+    handleUrls(process.argv)
+
+    store.dispatch(preboot())
+  })
+
+  app.on('activate', () => {
+    store.dispatch(focusWindow())
+  })
+
+  app.on('before-quit', (e) => {
+    store.dispatch(prepareQuit())
+  })
+
+  app.on('window-all-closed', (e) => {
+    const state = store.getState()
+    if (state.ui.mainWindow.quitting) {
+      // let normal electron shutdown process continue
+      return
+    } else {
+      // prevent electron shutdown, we want to remain alive
+      e.preventDefault()
+    }
+  })
+
+  // OSX (Info.pList)
+  app.on('open-url', (e, url) => {
+    if (isItchioURL(url)) {
+      // OSX will err -600 if we don't
+      e.preventDefault()
+      store.dispatch(openUrl(url))
+    } else {
+      console.log(`Ignoring non-itchio url: ${url}`)
+    }
+  })
+
+  // URL handling
+
+  function handleUrls (argv) {
+    // Windows (reg.exe), Linux (XDG)
+    argv.forEach((arg) => {
+      // XXX should we limit to one url at most ?
+      if (isItchioURL(arg)) {
+        store.dispatch(openUrl(arg))
+      }
+    })
+  }
+}
