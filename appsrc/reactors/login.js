49ce7631711416b98ea984894b80d43514fe5d23
Amos Wenger <amoswenger@gmail.com>
2016-06-16 19:44:25 +0200

    finish porting login saga

--- appsrc/reactors/login.js
+++ appsrc/reactors/login.js
@@ -2,6 +2,23 @@
 import * as actions from '../actions'
 import client from '../util/api'
 
+async function loginWithPassword (store, action) {
+  const {username, password} = action.payload
+
+  store.dispatch(actions.attemptLogin())
+
+  try {
+    const key = await getKey(username, password)
+    const keyClient = client.withKey(key)
+
+    // validate API key and get user profile in one fell swoop
+    const me = (await keyClient.me()).user
+    store.dispatch(actions.loginSucceeded({key, me}))
+  } catch (e) {
+    store.dispatch(actions.loginFailed({username, errors: e.errors || e.stack || e}))
+  }
+}
+
 async function loginWithToken (store, action) {
   const {username, key} = action.payload
 
@@ -23,4 +40,9 @@ async function loginWithToken (store, action) {
   }
 }
 
-export default {loginWithToken}
+async function getKey (username, password) {
+  const res = await client.loginWithPassword(username, password)
+  return res.key.key
+}
+
+export default {loginWithPassword, loginWithToken}
