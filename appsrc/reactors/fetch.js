54345d39a77f66c33919563a4a0d017ad6152977
Amos Wenger <amoswenger@gmail.com>
2016-06-16 17:26:52 +0200

    port fetch saga

---
+++ appsrc/reactors/fetch.js
@@ -0,0 +1,135 @@
+
+import Promise from 'bluebird'
+import invariant from 'invariant'
+
+import {createSelector} from 'reselect'
+
+import mklog from '../util/log'
+const log = mklog('fetch-reactor')
+import {opts} from '../logger'
+
+import {getUserMarket} from './market'
+import fetch from '../util/fetch'
+
+import {map, isEqual} from 'underline'
+
+import * as actions from '../actions'
+
+async function windowFocusChanged (store, action) {
+  const {focused} = action.payload
+  if (!focused) {
+    // app just went in the background, nbd
+    return
+  }
+
+  const credentials = store.getState().session.credentials
+  if (!credentials.key) {
+    log(opts, 'Not logged in, not fetching anything yet')
+    return
+  }
+
+  await fetchUsuals(credentials)
+}
+
+async function loginSucceeded (store, action) {
+  const credentials = action.payload
+  await fetchUsuals(credentials)
+}
+
+async function purchaseCompleted (store, action) {
+  const credentials = store.getState().session.credentials
+  if (!credentials.key) {
+    log(opts, 'Not logged in, not fetching anything yet')
+    return
+  }
+
+  await fetchUsuals(credentials)
+}
+
+async function fetchUsuals (credentials) {
+  invariant(credentials.key, 'have API key')
+
+  log(opts, 'Fetching the usuals')
+
+  const market = getUserMarket()
+
+  await Promise.all([
+    fetch.dashboardGames(market, credentials),
+    fetch.ownedKeys(market, credentials),
+    fetch.collections(market, credentials)
+  ])
+}
+
+async function search (store, action) {
+  // TODO: 200ms debounce
+
+  store.dispatch(actions.searchStarted())
+
+  try {
+    const credentials = store.getState().session.credentials
+    if (!credentials.key) {
+      log(opts, 'Not logged in, can\'t search')
+      return
+    }
+
+    const query = action.payload
+    if (!query) {
+      log(opts, 'Clearing query')
+      store.dispatch(actions.searchFetched({query: '', results: null}))
+      return
+    }
+
+    const results = await fetch.search(credentials, query)
+    store.dispatch(actions.searchFetched({query, results}))
+  } catch (e) {
+    // TODO: relay search error (network offline, etc.)
+  } finally {
+    store.dispatch(actions.searchFinished())
+  }
+}
+
+async function fetchSingleCollectionGames (store, market, credentials, collectionId) {
+  await fetch.collectionGames(market, credentials, collectionId)
+  store.dispatch(actions.collectionGamesFetched({collectionId}))
+}
+
+async function fetchCollectionGames (store, action) {
+  // TODO: 300ms debounce
+
+  const credentials = store.getState().session.credentials
+  if (!credentials.key) {
+    return
+  }
+
+  const market = getUserMarket()
+  const collections = market.getEntities('collections')
+
+  for (const key in Object.keys(collections)) {
+    await fetchSingleCollectionGames(store, market, credentials, Number(key))
+  }
+}
+
+const makeCollectionsWatcher = (store) => {
+  let oldIds = []
+
+  return createSelector(
+    (state) => state.market.collections,
+    (collections) => {
+      const ids = collections::map((c, id) => id)
+      if (!ids::isEqual(oldIds)) {
+        oldIds = ids
+        store.dispatch(fetchCollectionGames())
+      }
+    }
+  )
+}
+let collectionsWatcher
+
+async function userDbCommit (store, action) {
+  if (!collectionsWatcher) {
+    collectionsWatcher = makeCollectionsWatcher(store)
+  }
+  collectionsWatcher(store.getState())
+}
+
+export default {windowFocusChanged, loginSucceeded, purchaseCompleted, fetchCollectionGames, userDbCommit, search}
--- appsrc/reactors/index.js
+++ appsrc/reactors/index.js
@@ -7,20 +7,30 @@ import preferences from './preferences'
 import login from './login'
 import market from './market'
 import mainWindow from './main-window'
+import fetch from './fetch'
 
 export default validateReactors({
   PREBOOT: combine(preboot),
   BOOT: combine(market.boot, preferences.boot, mainWindow.focusWindow),
 
   LOGIN_WITH_TOKEN: combine(login.loginWithToken),
-  LOGIN_SUCCEEDED: combine(market.loginSucceeded),
+  LOGIN_SUCCEEDED: combine(market.loginSucceeded, fetch.loginSucceeded),
   LOGOUT: combine(market.logout),
+
+  USER_DB_COMMIT: combine(fetch.fetchCollectionGames),
 
   UPDATE_PREFERENCES: combine(preferences.updatePreferences),
 
   WINDOW_BOUNDS_CHANGED: combine(mainWindow.windowBoundsChanged),
+  WINDOW_FOCUS_CHANGED: combine(fetch.windowFocusChanged),
   FOCUS_WINDOW: combine(mainWindow.focusWindow),
   HIDE_WINDOW: combine(mainWindow.hideWindow),
+
+  PURCHASE_COMPLETED: combine(fetch.purchaseCompleted),
+
+  FETCH_COLLECTION_GAMES: combine(fetch.fetchCollectionGames),
+
+  SEARCH: combine(fetch.search),
 
   CLOSE_TAB_OR_AUX_WINDOW: combine(mainWindow.closeTabOrAuxWindow),
   QUIT_WHEN_MAIN: combine(mainWindow.quitWhenMain),
