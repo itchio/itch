0c9969ce67782fd5b9f3793c4b0105776e166e5f
Amos Wenger <amoswenger@gmail.com>
2016-07-07 20:57:14 +0200

    :fire: Remove a few unnecessary strings

--- appsrc/constants/static-tab-data.js
+++ appsrc/constants/static-tab-data.js
@@ -7,9 +7,8 @@ export default {
   collections: { label: ['sidebar.collections'] },
   press: { label: ['sidebar.press'], subtitle: ['sidebar.press_subtitle'] },
   library: { label: ['sidebar.owned'], subtitle: ['sidebar.owned_subtitle'] },
-  preferences: { label: ['sidebar.preferences'], subtitle: ['sidebar.preferences_subtitle'] },
-  history: { label: ['sidebar.history'], subtitle: ['sidebar.history_subtitle'] },
-  // downloads: { label: ['sidebar.downloads'], subtitle: ['sidebar.downloads_subtitle'] }
+  preferences: { label: ['sidebar.preferences'] },
+  history: { label: ['sidebar.history'] },
   downloads: { label: ['sidebar.downloads'] }
 }::map((data, id) => {
   return {...data, id, path: id}
