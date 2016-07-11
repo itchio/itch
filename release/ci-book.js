430af4a177dedf4f60fb7809a695e540a7e5904c
Amos Wenger <amoswenger@gmail.com>
2016-07-11 19:32:21 +0200

    Port release/ci-book to js (also full release/common module)

---
+++ release/ci-book.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+// generate latest documentation for itch using gitbook
+// and deploy it to google cloud storage.
+
+const $ = require('./common')
+
+$.show_versions(['npm', 'gsutil'])
+$($.npm_dep('gitbook', 'gitbook-cli'))
+
+$.cd('docs', function () {
+  $($.npm('install'))
+  $($.sh('gitbook build'))
+  $($.gcp(`_book/* gs://docs.itch.ovh/itch/${$.build_ref_name()}`))
+})
--- release/common.js
+++ release/common.js
@@ -1,9 +1,15 @@
 // base functions useful throughout CI scripts
 
 const child_process = require('child_process')
+const fs = require('fs')
+const ospath = require('path')
 const colors = require('colors')
 
-const $ = {}
+const $ = function (val) {
+  if (!val) {
+    throw new Error('Exit code assertion failed, bailing out')
+  }
+}
 
 $.HOMEPAGE = 
 $.HOMEPAGE = 'https://itch.io/app'
@@ -56,8 +62,8 @@ $.putln = function (s) {
 
 $.show_versions = function (names) {
   names.forEach(function (name) {
-    const v =  $.♫($.VERSION_SPECS[name]).trim()
-    $.putln `★ ${name} ${v}`.yellow
+    const v = $.get_output($.VERSION_SPECS[name]).trim()
+    $.putln(`★ ${name} ${v}`.yellow)
   })
   return true
 }
@@ -67,7 +73,8 @@ $.say = function (cmd) {
   return true
 }
 
-function system (cmd) {
+function system (cmd, opts = {}) {
+  const {quiet = true} = opts
   try {
     child_process.execSync(cmd, {
       stdio: 'inherit'
@@ -119,6 +126,157 @@ $.gothub = function (args) {
   process.env.GITHUB_USER = 'itchio'
   process.env.GITHUB_REPO = $.app_name
   return $.sh `gothub`
+}
+
+$.go_dep = function (cmd, pkg) {
+  if (system(`which ${cmd} > /dev/null`)) {
+    $.putln(`★ got ${cmd}`.yellow)
+    return true
+  } else {
+    $.putln(`☁ installing ${cmd}`.yellow)
+    return $.go(`get ${pkg}`)
+  }
+}
+
+$.gem_dep = function (cmd, pkg) {
+  if (system(`which ${cmd} > /dev/null`)) {
+    $.putln(`★ got ${cmd}`.yellow)
+    return true
+  } else {
+    $.putln(`☁ installing ${cmd}`.yellow)
+    return $.gem(`install ${pkg}`)
+  }
+}
+
+$.npm_dep = function (cmd, pkg) {
+  if (system(`which ${cmd} > /dev/null`)) {
+    $.putln(`★ got ${cmd}`.yellow)
+    return true
+  } else {
+    $.putln(`☁ installing ${cmd}`.yellow)
+    return $.npm(`install -g ${pkg}`)
+  }
+}
+
+$.ensure = $
+
+$.retry = function (cb) {
+  var tries = 0
+  while (tries < $.RETRY_COUNT) {
+    if (tries > 0) {
+      $.say(`Command failed, waiting 30s then trying ${$.RETRY_COUNT - tries} more time(s).`)
+      // naughty, but don't want to pull in node-sleep (native modules)
+      // or turn everything into async (babel-cli = huge)
+      system('sleep 30')
+    }
+    if (cb()) {
+      // cmd returned truthy value, was successful
+      return
+    }
+    tries++
+  }
+  throw new Exception(`Tried ${RETRY_COUNT} times, bailing out`)
+}
+
+$.get_output = function (cmd) {
+  const out = child_process.execSync(cmd, {
+    encoding: 'utf8'
+  })
+  // console.log('cmd = ', cmd)
+  // console.log('out = ', out)
+  return out
+}
+
+$.prompt = function (msg) {
+  process.stdout.write(`${msg} (y/n)`)
+  var s = 'n'
+  fs.readSync(process.stdin, s, 0, 1, 0)
+  process.stdout.write('\n')
+
+  if (s !== 'y') {
+    $.say('Bailing out...')
+    process.exit(0)
+  }
+}
+
+$.cd = function (dir, cb) {
+  const original_wd = ospath.resolve(__dirname, '..')
+  var e
+  var ret
+
+  $.putln(`☞ entering ${dir}`)
+  process.chdir(dir)
+  try {
+    ret = cb()
+  } catch (err) {
+    e = err
+  } finally {
+    $.putln(`☜ leaving ${dir}`)
+    process.chdir(original_wd)
+  }
+
+  if (e) {
+    throw e
+  }
+  return ret
+}
+
+// environment variables etc.
+
+$.build_ref_name = function () {
+  const v = process.env.CI_BUILD_REF_NAME
+  if (!v) {
+    throw new Error('No build ref!')
+  }
+  return v
+}
+
+$.build_tag = function () {
+  const v = process.env.CI_BUILD_TAG
+  if (!v) {
+    throw new Error('No build tag!')
+  }
+  return v
+}
+
+$.build_version = function () {
+  return $.build_tag().replace(/^v/, '').replace(/-.+$/, '')
+}
+
+$.app_name = function () {
+  if (/-canary$/.test($.build_tag())) {
+    return 'kitch'
+  } else {
+    return 'itch'
+  }
+}
+
+$.channel_name = function () {
+  if (/-canary$/.test($.build_tag())) {
+    return 'canary'
+  } else {
+    return 'stable'
+  }
+}
+
+$.to_deb_arch = function (arch) {
+  switch (arch) {
+    case '386': return 'i386'
+    case 'amd64': return 'amd64'
+    default: throw new Error(`Unsupported arch ${arch}`)
+  }
+}
+
+$.to_rpm_arch = function (arch) {
+  switch (arch) {
+    case '386': return 'i386'
+    case 'amd64': return 'x86_64'
+    default: throw new Error(`Unsupported arch ${arch}`)
+  }
+}
+
+$.build_time = function () {
+  return $.BUILD_TIME
 }
 
 module.exports = $
