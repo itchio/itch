9955350456b768ae7b53f16365f972b0b5c0e0d7
Amos Wenger <amoswenger@gmail.com>
2016-11-04 14:20:47 +0100

    woop, had it backwards

--- scripts/port-progress.sh
+++ scripts/port-progress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 printf "typescript: "
-wc -l $(find appsrc -name '*.js') | tail -1
-printf "javascript: "
 wc -l $(find appsrcts -name '*.ts') | tail -1
+printf "javascript: "
+wc -l $(find appsrc -name '*.js') | tail -1
