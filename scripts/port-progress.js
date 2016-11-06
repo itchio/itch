0d8afde3876506659ac6f752b84b96b5271abe20
Amos Wenger <amoswenger@gmail.com>
2016-11-06 16:46:08 +0100

    :shirt: scripts/port-progress, heh

--- scripts/port-progress.js
+++ scripts/port-progress.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-let childProcess = require('child_process');
+let childProcess = require('child_process')
 
-const ts = +String(childProcess.execSync("wc -l $(find appsrcts -name '*.ts') | tail -1")).replace("total", "").trim();
-const js = +String(childProcess.execSync("wc -l $(find appsrc -name '*.js') | tail -1")).replace("total", "").trim();
-const total = ts + js;
+const ts = +String(childProcess.execSync('wc -l $(find appsrcts -name "*.ts") | tail -1')).replace('total', '').trim()
+const js = +String(childProcess.execSync('wc -l $(find appsrc -name "*.js") | tail -1')).replace('total', '').trim()
+const total = ts + js
 
 console.log(`typescript: ${ts} lines`)
 console.log(`javascript: ${js} lines`)
