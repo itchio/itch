14a14a39b0126b03af2ba4306fe3bcd8f59da11f
Philip Karpiak <philip@eswat.ca>
2016-11-07 14:01:40 -0500

    Fix #840 - show notification on install or update

--- appsrc/reactors/tasks/download-ended.ts
+++ appsrc/reactors/tasks/download-ended.ts
@@ -5,6 +5,9 @@ import {log, opts} from "./log";
 
 import {omit} from "underscore";
 
+import * as actions from "../../actions";
+import localizer from "../../localizer";
+
 import {IStore} from "../../types";
 
 import {IAction, IDownloadEndedPayload} from "../../constants/action-types";
@@ -47,6 +50,18 @@ export async function downloadEnded (store: IStore, action: IAction<IDownloadEnd
         log(opts, `Error in install: ${installErr}`);
         return;
       }
+
+      const i18n = store.getState().i18n;
+      const t = localizer.getT(i18n.strings, i18n.lang);
+      const message = t(
+        reason === "install"
+        ? "notification.download_installed"
+        : "notification.download_updated"
+        , {title: downloadOpts.game.title}
+      )
+      store.dispatch(actions.notify({
+        body: message,
+      }));
     }
   } else {
     log(opts, `Downloaded something for reason ${reason}`);
