9fc4d36a83fa3662d28b30ffc2109fec1b0c8c0c
Amos Wenger <amoswenger@gmail.com>
2016-11-15 15:47:27 +0100

    Bump up log file size before rotation

--- appsrc/util/log.ts
+++ appsrc/util/log.ts
@@ -92,8 +92,8 @@ export class Logger {
 
             this.fileSink = stream({
               file: val,
-              size: "300K",
-              keep: 3,
+              size: "2M",
+              keep: 5,
             });
           }
           break;
