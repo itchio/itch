06e2dab1e6dfff48973a984e4b6dfba7bb30bfee
Amos Wenger <amoswenger@gmail.com>
2016-12-09 11:24:28 +0100

    clean up derived reducer.. somewhat

--- appsrc/reducers/derived-reducer.ts
+++ appsrc/reducers/derived-reducer.ts
@@ -1,10 +1,17 @@
 
 import {Reducer, Action} from "redux";
 
-export default function derived <T> (reducer: Reducer<T>, derivedReducer: (state: T) => T): Reducer<T> {
+export default function derived <T> (
+    reducer: Reducer<any>, derivedReducer: (state: any) => any): Reducer<T> {
   return (state: T, action: Action) => {
     const reducerFields = reducer(state, action);
-    const derivedFields = state ? derivedReducer(reducerFields) : {};
-    return Object.assign({}, reducerFields, derivedFields);
+    if (state) {
+      return {
+        ...reducerFields,
+        ...derivedReducer(reducerFields),
+      };
+    } else {
+      return reducerFields;
+    }
   };
 }
