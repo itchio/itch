3feff3c73d211e017f13c700cde2ae6f280ef910
Amos Wenger <amoswenger@gmail.com>
2016-12-09 11:46:51 +0100

    clean up credentials reducer, cf. #955

--- appsrc/reducers/session/credentials.ts
+++ appsrc/reducers/session/credentials.ts
@@ -1,26 +1,24 @@
 
-import {handleActions} from "redux-actions";
-
 import {ISessionCredentialsState} from "../../types";
-
-import {
-  IAction,
-  ILoginSucceededPayload,
-  ILogoutPayload,
-} from "../../constants/action-types";
+import * as actions from "../../actions";
+import reducer from "../reducer";
 
 const initialState = {
   key: null,
   me: null,
 } as ISessionCredentialsState;
 
-export default handleActions<ISessionCredentialsState, any>({
-  LOGIN_SUCCEEDED: (state: ISessionCredentialsState, action: IAction<ILoginSucceededPayload>) => {
+export default reducer<ISessionCredentialsState>(initialState, (on) => {
+  on(actions.loginSucceeded, (state, action) => {
     const {key, me} = action.payload;
-    return Object.assign({}, state, {key, me});
-  },
+    return {
+      ...state,
+      key,
+      me,
+    };
+  });
 
-  LOGOUT: (state: ISessionCredentialsState, action: IAction<ILogoutPayload>) => {
-    return Object.assign({}, state, {key: null, me: null});
-  },
-}, initialState);
+  on(actions.logout, (state, action) => {
+    return initialState;
+  });
+});
