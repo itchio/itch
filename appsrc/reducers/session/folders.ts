46de38307994a72326d1eef716eb21bea080edfe
Amos Wenger <amoswenger@gmail.com>
2016-12-09 11:29:50 +0100

    Clean up folders reducer, cf. #955

--- appsrc/reducers/session/folders.ts
+++ appsrc/reducers/session/folders.ts
@@ -1,26 +1,23 @@
 
-import {handleActions} from "redux-actions";
 import * as ospath from "path";
 import {app} from "../../electron";
 
 import {ISessionFoldersState} from "../../types";
-
-import {
-  IAction, ILoginSucceededPayload, ILogoutPayload,
-} from "../../constants/action-types";
+import * as actions from "../../actions";
+import reducer from "../reducer";
 
 const initialState = {
   libraryDir: null,
 } as ISessionFoldersState;
 
-export default handleActions<ISessionFoldersState, any>({
-  LOGIN_SUCCEEDED: (state: ISessionFoldersState, action: IAction<ILoginSucceededPayload>) => {
+export default reducer<ISessionFoldersState>(initialState, (on) => {
+  on(actions.loginSucceeded, (state, action) => {
     const {me} = action.payload;
     const libraryDir = ospath.join(app.getPath("userData"), "users", "" + me.id);
-    return Object.assign({}, state, {libraryDir});
-  },
+    return {...state, libraryDir};
+  });
 
-  LOGOUT: (state: ISessionFoldersState, action: IAction<ILogoutPayload>) => {
+  on(actions.logout, (state, action) => {
     return initialState;
-  },
-}, initialState);
+  });
+});
