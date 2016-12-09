25ced6f79241102bfe43de1cd2ee6dc595a67d6b
Amos Wenger <amoswenger@gmail.com>
2016-12-09 11:04:55 +0100

    clean up setup reducer, cf. #955

--- appsrc/reducers/setup.ts
+++ appsrc/reducers/setup.ts
@@ -1,13 +1,7 @@
 
-import {handleActions} from "redux-actions";
-
 import {ISetupState} from "../types";
-
-import {
-  IAction,
-  ISetupStatusPayload,
-  ISetupDonePayload,
-} from "../constants/action-types";
+import * as actions from "../actions";
+import reducer from "./reducer";
 
 const initialState = {
   done: false,
@@ -15,19 +9,22 @@ const initialState = {
   blockingOperation: null,
 } as ISetupState;
 
-export default handleActions<ISetupState, any>({
-  SETUP_STATUS: (state: ISetupState, action: IAction<ISetupStatusPayload>) => {
-    return Object.assign({}, state, {
+export default reducer<ISetupState>(initialState, (on) => {
+  on(actions.setupStatus, (state, action) => {
+    const blockingOperation = action.payload;
+    return {
+      ...state,
       errors: [],
-      blockingOperation: action.payload,
-    });
-  },
+      blockingOperation,
+    };
+  });
 
-  SETUP_DONE: (state: ISetupState, action: IAction<ISetupDonePayload>) => {
-    return Object.assign({}, state, {
+  on(actions.setupDone, (state, action) => {
+    return {
+      ...state,
       done: true,
       errors: [],
       blockingOperation: null,
-    });
-  },
-}, initialState);
+    };
+  });
+});
