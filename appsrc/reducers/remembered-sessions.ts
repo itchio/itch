7bd0165b6d48fb218511d5fdc8f8aa4ea431647c
Amos Wenger <amoswenger@gmail.com>
2016-12-09 10:53:40 +0100

    Clean up remembered sessions reducer, cf. #955

--- appsrc/reducers/remembered-sessions.ts
+++ appsrc/reducers/remembered-sessions.ts
@@ -1,36 +1,32 @@
 
-import * as invariant from "invariant";
 import {omit} from "underscore";
-import {handleActions} from "redux-actions";
 
-import {
-  IAction,
-  ISessionsRememberedPayload,
-  ISessionUpdatedPayload,
-  IForgetSessionPayload,
-} from "../constants/action-types";
+import * as actions from "../actions";
+import reducer from "./reducer";
 
 import {IRememberedSessionsState} from "../types";
 
 const initialState = {} as IRememberedSessionsState;
 
-export default handleActions<IRememberedSessionsState, any>({
-  SESSIONS_REMEMBERED: (state: IRememberedSessionsState, action: IAction<ISessionsRememberedPayload>) => {
+export default reducer<IRememberedSessionsState>(initialState, (on) => {
+  on(actions.sessionsRemembered, (state, action) => {
     const sessions = action.payload;
     return sessions;
-  },
+  });
 
-  SESSION_UPDATED: (state: IRememberedSessionsState, action: IAction<ISessionUpdatedPayload>) => {
+  on(actions.sessionUpdated, (state, action) => {
     const {id, record} = action.payload;
-    const session = state[id] || {};
-    return Object.assign({}, state, {
-      [id]: Object.assign({}, session, record),
-    });
-  },
+    return {
+      ...state,
+      [id]: {
+        ...(state[id] || {}),
+        ...record,
+      },
+    };
+  });
 
-  FORGET_SESSION: (state: IRememberedSessionsState, action: IAction<IForgetSessionPayload>) => {
+  on(actions.forgetSession, (state, action) => {
     const {id} = action.payload;
-    invariant(typeof id !== "undefined", "forgetting session from a valid userId");
     return omit(state, "" + id);
-  },
-}, initialState);
+  });
+});
