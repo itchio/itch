97a47c59f11eb00e4fc183466f149955df6b513c
Amos Wenger <amoswenger@gmail.com>
2017-01-27 16:23:03 +0100

    Prettier table view

--- appsrc/components/game-table.tsx
+++ appsrc/components/game-table.tsx
@@ -13,14 +13,35 @@ import * as actions from "../actions";
 import {AutoSizer, Table, Column} from "react-virtualized";
 import {IAutoSizerParams} from "./autosizer-types";
 
+import NiceAgo from "./nice-ago";
+
 interface IRowGetterParams {
   index: number;
+}
+
+interface ICellRendererParams {
+  cellData: IGameRecord;
+  columnData: any;
+  dataKey: string;
+  isScrolling: boolean;
+  rowData: any;
+  rowIndex: number;
+}
+
+interface ICellDataGetter {
+  columnData: any;
+  dataKey: string;
+  rowData: any;
 }
 
 class GameTable extends React.Component<IGameTableProps, void> {
   constructor() {
     super();
     this.rowGetter = this.rowGetter.bind(this);
+    this.coverRenderer = this.coverRenderer.bind(this);
+    this.titleRenderer = this.titleRenderer.bind(this);
+    this.publishedAtRenderer = this.publishedAtRenderer.bind(this);
+    this.genericDataGetter = this.genericDataGetter.bind(this);
   }
 
   rowGetter (params: IRowGetterParams): any {
@@ -28,24 +49,77 @@ class GameTable extends React.Component<IGameTableProps, void> {
     return this.props.games[index];
   }
 
+  genericDataGetter (params: ICellDataGetter): any {
+    return params.rowData;
+  }
+
+  coverRenderer (params: ICellRendererParams): JSX.Element | string {
+    const {cellData} = params;
+    return <div className="cover" style={{backgroundImage: `url("${cellData.stillCoverUrl || cellData.coverUrl}")`}}/>;
+  }
+
+  titleRenderer (params: ICellRendererParams): JSX.Element | string {
+    const {cellData} = params;
+    return <div className="title-column" onClick={(e) => {
+        this.props.navigateToGame(cellData);
+      }}>
+      <div className="title">{cellData.title}</div>
+      <div className="description">{cellData.shortText}</div>
+    </div>;
+  }
+
+  publishedAtRenderer (params: ICellRendererParams): JSX.Element | string {
+    const {cellData} = params;
+    const {publishedAt} = cellData;
+    if (publishedAt) {
+      return <NiceAgo date={publishedAt}/>;
+    } else {
+      return "";
+    }
+  }
+
   render () {
     const {t, games} = this.props;
 
-    return <AutoSizer>
-      {({width, height}: IAutoSizerParams) => (
-        <Table
-            headerHeight={40}
+    return <div className="hub-game-table">
+      <AutoSizer>
+      {({width, height}: IAutoSizerParams) => {
+        let remainingWidth = width;
+        let coverWidth = 50;
+        remainingWidth -= coverWidth;
+
+        let publishedWidth = 150;
+        remainingWidth -= publishedWidth;
+
+        return <Table
+            headerHeight={30}
             height={height}
             width={width}
             rowCount={games.length}
-            rowHeight={40}
+            rowHeight={55}
             rowGetter={this.rowGetter}
           >
-          <Column dataKey="title" label={t("table.column.name")} width={width * .5}/>
-          <Column dataKey="publishedAt" label={t("table.column.published")} width={width * .5}/>
-        </Table>
-      )}
-    </AutoSizer>;
+          <Column
+            dataKey="cover"
+             width={coverWidth}
+             cellDataGetter={this.genericDataGetter}
+             cellRenderer={this.coverRenderer}/>
+          <Column
+            dataKey="title"
+             label={t("table.column.name")}
+             width={remainingWidth}
+             cellDataGetter={this.genericDataGetter}
+             cellRenderer={this.titleRenderer}/>
+           <Column
+             dataKey="publishedAt"
+             label={t("table.column.published")}
+             width={publishedWidth}
+             cellDataGetter={this.genericDataGetter}
+             cellRenderer={this.publishedAtRenderer}/>
+        </Table>;
+      }}
+    </AutoSizer>
+    </div>;
   }
 }
 
@@ -60,6 +134,7 @@ interface IGameTableProps {
   t: ILocalizer;
 
   clearFilters: typeof actions.clearFilters;
+  navigateToGame: typeof actions.navigateToGame;
 }
 
 const mapStateToProps = (initialState: IState, props: IGameTableProps) => {
@@ -73,6 +148,7 @@ const mapStateToProps = (initialState: IState, props: IGameTableProps) => {
 
 const mapDispatchToProps = (dispatch: (action: IAction<any>) => void) => ({
   clearFilters: dispatcher(dispatch, actions.clearFilters),
+  navigateToGame: dispatcher(dispatch, actions.navigateToGame),
 });
 
 export default connect(
