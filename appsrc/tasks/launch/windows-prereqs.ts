ad051f359cfb0f16d20fa5e4da42034399162f24
Amos Wenger <amoswenger@gmail.com>
2017-02-01 16:51:44 +0100

    On second thought, don't let UE4 prereqs be quiet, because they might take a long while

--- appsrc/tasks/launch/windows-prereqs.ts
+++ appsrc/tasks/launch/windows-prereqs.ts
@@ -96,7 +96,6 @@ async function handleUE4Prereq (cave: ICaveRecord, opts: IWindowsPrereqsOpts) {
     const code = await spawn({
       command: prereqFullPath,
       args: [
-        "/quiet", // don't show any dialogs
         "/norestart", // don't ask for OS to reboot
       ],
       onToken: (tok) => log(opts, `[ue4-prereq out] ${tok}`),
