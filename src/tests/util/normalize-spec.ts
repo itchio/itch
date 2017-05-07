85e98805cb475c2acc722592f1a779017a804465
Amos Wenger <amos@itch.io>
2017-05-07 21:20:00 +0200

    fix normalize spec

--- src/tests/util/normalize-spec.ts
+++ src/tests/util/normalize-spec.ts
@@ -16,7 +16,7 @@ test("normalize", t => {
 
   t.case("normalize collection games", async t => {
     const apiResponse = fixture.json("api/collection/27968/games");
-    const result = normalize(collectionGamesSchema, apiResponse);
+    const result = normalize(apiResponse, collectionGamesSchema);
     t.true(result.entities.games);
     const g = result.entities.games["72440"];
     t.is(g.canBeBought, true);
