f173d151f37b2a49bed5b24b18f60e230b1b7c0d
Amos Wenger <amos@itch.io>
2017-05-23 11:29:38 +0200

    When routing in test, consider reactor errors fatal, cache emptyArr to avoid unnecessary allocations

--- src/reactors/route.ts
+++ src/reactors/route.ts
@@ -4,42 +4,57 @@ import {IAction} from "../constants/action-types";
 
 import {Watcher} from "./watcher";
 
+import env from "../env";
+
 import rootLogger from "../logger";
 const logger = rootLogger.child({name: "route"});
+
+let err = (msg: string) => {
+  logger.error(msg);
+};
+
+if (env.name === "test") {
+  err = (msg: string) => {
+    logger.error(msg);
+    logger.error("Bailing out...");
+    process.exit(1);
+  };
+}
+
+const emptyArr = [];
 
 export default async function route (watcher: Watcher, store: IStore, action: IAction<any>) {
   setTimeout(() => {
     try {
-      for (const r of (watcher.reactors[action.type] || [])) {
+      for (const r of (watcher.reactors[action.type] || emptyArr)) {
         r(store, action).catch((e) => {
-          logger.error(`while reacting to ${(action || {type: "?"}).type}: ${e.stack || e}`);
+          err(`while reacting to ${(action || {type: "?"}).type}: ${e.stack || e}`);
         });
       }
 
-      for (const r of (watcher.reactors._ALL || [])) {
+      for (const r of (watcher.reactors._ALL || emptyArr)) {
         r(store, action).catch((e) => {
-          logger.error(`while reacting to ${(action || {type: "?"}).type}: ${e.stack || e}`);
+          err(`while reacting to ${(action || {type: "?"}).type}: ${e.stack || e}`);
         });
       }
 
       for (const sub of watcher.subs) {
         if (!sub) { continue; }
 
-        for (const r of (sub.reactors[action.type] || [])) {
+        for (const r of (sub.reactors[action.type] || emptyArr)) {
           r(store, action).catch((e) => {
-            logger.error(`while reacting to ${(action || {type: "?"}).type}: ${e.stack || e}`);
+            err(`while reacting to ${(action || {type: "?"}).type}: ${e.stack || e}`);
           });
         }
 
-        for (const r of (sub.reactors._ALL || [])) {
+        for (const r of (sub.reactors._ALL || emptyArr)) {
           r(store, action).catch((e) => {
-            logger.error(`while reacting to ${(action || {type: "?"}).type}: ${e.stack || e}`);
+            err(`while reacting to ${(action || {type: "?"}).type}: ${e.stack || e}`);
           });
         }
       }
     } catch (e) {
-      // tslint:disable-next-line
-      console.log(`Could not route ${action.type}: ${e.stack}`);
+      err(`Could not route ${action.type}: ${e.stack}`);
     }
-  }, 0)
+  }, 0);
 }
