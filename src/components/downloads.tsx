64c48496209fd8551b606187fe2532cf92c32799
Amos Wenger <amoswenger@gmail.com>
2017-06-03 12:07:09 +0200

    Add titlebar to downloads tab

--- src/components/downloads.tsx
+++ src/components/downloads.tsx
@@ -7,6 +7,7 @@ import {map, first, rest} from "underscore";
 import * as actions from "../actions";
 
 import DownloadRow from "./download-row";
+import TitleBar from "./title-bar";
 
 import {IAppState, IDownloadItem} from "../types";
 import {dispatcher} from "../constants/action-types";
@@ -17,7 +18,9 @@ import styled, * as styles from "./styles";
 
 const DownloadsDiv = styled.div`
   ${styles.meat()}
+`;
 
+const DownloadsContentDiv = styled.div`
   overflow-y: auto;
   padding: 0 20px 20px 20px;
 
@@ -57,58 +60,66 @@ class Downloads extends React.PureComponent<IProps & IDerivedProps & I18nProps, 
   }
 
   render () {
+    const {tab} = this.props;
+
+    return <DownloadsDiv>
+      <TitleBar tab={tab}/>
+      <DownloadsContentDiv>{this.renderContents()}</DownloadsContentDiv>
+    </DownloadsDiv>;
+  }
+
+  renderContents () {
     const {t, items, finishedItems} = this.props;
     const {clearFinishedDownloads} = this.props;
 
     const hasItems = (items.length + finishedItems.length) > 0;
     if (!hasItems) {
       return <EmptyState>
-          {t("status.downloads.no_active_downloads")}
+        {t("status.downloads.no_active_downloads")}
       </EmptyState>;
     }
 
     const firstItem = first(items);
     const queuedItems = rest(items);
 
-    return <DownloadsDiv>
-    {firstItem
-    ? <div className="section-bar">
-      <h2>{t("status.downloads.category.active")}</h2>
-    </div>
-    : ""}
-
-    {firstItem
-    ? <DownloadRow key={firstItem.id} item={firstItem} first active/>
-    : ""}
-
-    {queuedItems.length > 0
-    ? <div className="section-bar">
-      <h2>{t("status.downloads.category.queued")}</h2>
-    </div>
-    : ""}
-    {queuedItems.length > 0
-    ? map(queuedItems, (item, i) =>
-      <DownloadRow key={item.id} item={item} active/>,
-    )
-    : ""}
-
-    {finishedItems.length > 0
-      ? [
-        <div className="section-bar">
-          <h2 className="finished-header">
-            {t("status.downloads.category.finished")}
-          </h2>
-          <span className="clear" data-rh-at="right" data-rh={t("status.downloads.clear_all_finished")}
-              onClick={() => clearFinishedDownloads({})}>
-            <span className="icon icon-delete"/>
-          </span>
-        </div>,
-      ].concat(map(finishedItems, (item) =>
-        <DownloadRow key={item.id} item={item}/>,
-      ))
+    return <DownloadsContentDiv>
+      {firstItem
+      ? <div className="section-bar">
+        <h2>{t("status.downloads.category.active")}</h2>
+      </div>
       : ""}
 
-    </DownloadsDiv>;
+      {firstItem
+      ? <DownloadRow key={firstItem.id} item={firstItem} first active/>
+      : ""}
+
+      {queuedItems.length > 0
+      ? <div className="section-bar">
+        <h2>{t("status.downloads.category.queued")}</h2>
+      </div>
+      : ""}
+      {queuedItems.length > 0
+      ? map(queuedItems, (item, i) =>
+        <DownloadRow key={item.id} item={item} active/>,
+      )
+      : ""}
+
+      {finishedItems.length > 0
+        ? [
+          <div className="section-bar">
+            <h2 className="finished-header">
+              {t("status.downloads.category.finished")}
+            </h2>
+            <span className="clear" data-rh-at="right" data-rh={t("status.downloads.clear_all_finished")}
+                onClick={() => clearFinishedDownloads({})}>
+              <span className="icon icon-delete"/>
+            </span>
+          </div>,
+        ].concat(map(finishedItems, (item) =>
+          <DownloadRow key={item.id} item={item}/>,
+        ))
+        : ""}
+    </DownloadsContentDiv>;
   }
 }
 
