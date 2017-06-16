fef089ed0aea233846a90d4caf803fb3c3db38cd
Amos Wenger <amos@itch.io>
2017-06-16 20:21:58 +0200

    introduce lazy-get-game to replace util/fetch

---
+++ src/reactors/lazy-get-game.ts
@@ -0,0 +1,28 @@
+
+import db from "../db";
+import Game from "../db/models/game";
+
+import client from "../api";
+
+import {IStore} from "../types";
+import {getGameCredentialsForId} from "./downloads/get-game-credentials";
+
+export default async function lazyGetGame(store: IStore, gameId: number): Promise<Game> {
+  const game = db.games.findOneById(gameId);
+  if (game) {
+    return game;
+  }
+
+  const gameCredentials = await getGameCredentialsForId(store, gameId);
+  if (!gameCredentials) {
+    return null;
+  }
+
+  const api = client.withKey(gameCredentials.apiKey);
+  const gameResponse = await api.game(gameId);
+  if (gameResponse) {
+    return gameResponse.game;
+  }
+
+  return null;
+}
