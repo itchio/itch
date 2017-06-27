3cb5498fb710097d50f20344f35c4c2b1316296c
Amos Wenger <amos@itch.io>
2017-06-27 13:33:09 +0200

    Run thorough tests in CI

--- release/ci-test.js
+++ release/ci-test.js
@@ -11,11 +11,11 @@ async function main() {
   $(await $.npm("install"));
 
   if (process.platform === "linux") {
-    $(await $.sh('xvfb-run -a -s "-screen 0 1280x720x24" npm test'));
+    $(await $.sh('xvfb-run -a -s "-screen 0 1280x720x24" npm test -- --thorough'));
     $(await $.sh('xvfb-run -a -s "-screen 0 1280x720x24" npm run integration-tests'));
     $(await $.npm("run upload-coverage"));
   } else {
-    $(await $.npm("test"));
+    $(await $.npm("test -- --thorough"));
     $(await $.npm("run integration-tests"));
   }
 }
