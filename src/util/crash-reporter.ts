400b483a9a586dbb63899c8f261dc3d69b120fda
Amos Wenger <amos@itch.io>
2017-06-27 13:36:41 +0200

    crash-reporter clean-up

--- src/util/crash-reporter.ts
+++ src/util/crash-reporter.ts
@@ -41,166 +41,152 @@ export interface IReportIssueOpts {
   before?: string;
 }
 
-let self = {
-  catching: false,
+let catching = false;
 
-  sampleCrash: () => {
-    setTimeout(() => {
-      throw new Error("hello this is crash reporter with a sample crash.");
-    }, 10);
-  },
+async function writeCrashLog(e: Error) {
+  const crashFile = path.join(
+    app.getPath("userData"),
+    "crash_logs",
+    `${+new Date()}.txt`,
+  );
 
-  writeCrashLog: async (e: Error) => {
-    const crashFile = path.join(
-      app.getPath("userData"),
-      "crash_logs",
-      `${+new Date()}.txt`,
-    );
+  let log = "";
+  log += e.stack || e.message || e;
 
-    let log = "";
-    log += e.stack || e.message || e;
+  if (os.platform() === "win32") {
+    log = log.replace(/\n/g, "\r\n");
+  }
+  await sf.writeFile(crashFile, log, { encoding: "utf8" });
 
-    if (os.platform() === "win32") {
-      log = log.replace(/\n/g, "\r\n");
-    }
-    await sf.writeFile(crashFile, log, { encoding: "utf8" });
+  return { log, crashFile };
+}
 
-    return { log, crashFile };
-  },
+function reportIssue(opts: IReportIssueOpts) {
+  if (typeof opts === "undefined") {
+    opts = {};
+  }
 
-  reportIssue: (opts: IReportIssueOpts) => {
-    if (typeof opts === "undefined") {
-      opts = {};
-    }
+  const log = opts.log;
+  let body = opts.body || "";
+  let type = opts.type || "Issue";
+  const repo = opts.repo || urls.itchRepo;
+  const before = opts.before || "";
 
-    const log = opts.log;
-    let body = opts.body || "";
-    let type = opts.type || "Issue";
-    const repo = opts.repo || urls.itchRepo;
-    const before = opts.before || "";
-
-    if (typeof log !== "undefined") {
-      type = "Feedback";
-      body = `Event log:
+  if (typeof log !== "undefined") {
+    type = "Feedback";
+    body = `Event log:
 
 \`\`\`
 ${log}
 \`\`\`
 `;
-    }
+  }
 
-    const platformEmoji = findWhere(platformData, {
-      platform: os.itchPlatform(),
-    }).emoji;
-    const query = querystring.stringify({
-      title: `${platformEmoji} ${type} v${app.getVersion()}`,
-      body: before + body,
-    });
-    let url = `${repo}/issues/new?${query}`;
-    const maxLen = 2000;
-    if (url.length > maxLen) {
-      url = url.substring(0, maxLen);
-    }
-    shell.openExternal(url);
-  },
+  const platformEmoji = findWhere(platformData, {
+    platform: os.itchPlatform(),
+  }).emoji;
+  const query = querystring.stringify({
+    title: `${platformEmoji} ${type} v${app.getVersion()}`,
+    body: before + body,
+  });
+  let url = `${repo}/issues/new?${query}`;
+  const maxLen = 2000;
+  if (url.length > maxLen) {
+    url = url.substring(0, maxLen);
+  }
+  shell.openExternal(url);
+}
 
-  handle: async function(type: ErrorType, e: Error) {
-    if (self.catching) {
-      // tslint:disable-next-line
-      console.log(`While catching: ${e.stack || e}`);
-      return;
-    }
-    self.catching = true;
-
+async function handle(type: ErrorType, e: Error) {
+  if (catching) {
     // tslint:disable-next-line
-    console.log(`${errorTypeString(type)}: ${e.stack}`);
-    let res = await self.writeCrashLog(e);
-    let log = res.log;
-    let crashFile = res.crashFile;
+    console.log(`While catching: ${e.stack || e}`);
+    return;
+  }
+  catching = true;
 
-    if (env.name === "test") {
-      // tslint:disable-next-line
-      console.log(`Crash log written to ${res.crashFile}, bailing out`);
-      os.exit(1);
-      return;
-    }
+  // tslint:disable-next-line
+  console.log(`${errorTypeString(type)}: ${e.stack}`);
+  let res = await writeCrashLog(e);
+  let log = res.log;
+  let crashFile = res.crashFile;
 
-    // TODO: something better
-    const t = require("../localizer").getT({}, "en");
-
-    const buttons = [
-      t("prompt.crash_reporter.report_issue", { defaultValue: "Report issue" }),
-      t("prompt.crash_reporter.open_crash_log", {
-        defaultValue: "Open crash log",
-      }),
-      t("prompt.action.close", { defaultValue: "Close" }),
-    ];
-    if (env.name === "development") {
-      buttons.push("Ignore and continue (dev only)");
-    }
-    let dialogOpts = {
-      type: "error" as "error", // woo typescript is crazy stuff, friendos
-      buttons,
-      message: t("prompt.crash_reporter.message", {
-        defaultValue: "The application has crashed",
-      }),
-      detail: t("prompt.crash_reporter.detail", {
-        defaultValue: `A crash log was written to ${crashFile}`,
-        location: crashFile,
-      }),
-    };
-
-    const response = await new Promise((resolve, reject) =>
-      dialog.showMessageBox(dialogOpts, resolve),
-    );
-
-    if (response === 0) {
-      self.reportIssue({ log });
-    } else if (response === 1) {
-      shell.openItem(crashFile);
-    } else if (response === 3) {
-      // ignore and continue
-      return;
-    }
+  if (env.name === "test") {
+    // tslint:disable-next-line
+    console.log(`Crash log written to ${res.crashFile}, bailing out`);
     os.exit(1);
+    return;
+  }
 
-    self.catching = false;
-  },
+  // TODO: something better
+  const t = require("../localizer").getT({}, "en");
 
-  mount: () => {
-    if (env.name === "test") {
+  const buttons = [
+    t("prompt.crash_reporter.report_issue", { defaultValue: "Report issue" }),
+    t("prompt.crash_reporter.open_crash_log", {
+      defaultValue: "Open crash log",
+    }),
+    t("prompt.action.close", { defaultValue: "Close" }),
+  ];
+  if (env.name === "development") {
+    buttons.push("Ignore and continue (dev only)");
+  }
+  let dialogOpts = {
+    type: "error" as "error", // woo typescript is crazy stuff, friendos
+    buttons,
+    message: t("prompt.crash_reporter.message", {
+      defaultValue: "The application has crashed",
+    }),
+    detail: t("prompt.crash_reporter.detail", {
+      defaultValue: `A crash log was written to ${crashFile}`,
+      location: crashFile,
+    }),
+  };
+
+  const response = await new Promise((resolve, reject) =>
+    dialog.showMessageBox(dialogOpts, resolve),
+  );
+
+  if (response === 0) {
+    reportIssue({ log });
+  } else if (response === 1) {
+    shell.openItem(crashFile);
+  } else if (response === 3) {
+    // ignore and continue
+    return;
+  }
+  os.exit(1);
+
+  catching = false;
+}
+
+function makeHandler(type: ErrorType) {
+  return (e: Error) => {
+    if (isNetworkError(e)) {
+      console.error(`Uncaught network error: ${e.stack}`);
       return;
     }
 
-    const makeHandler = (type: ErrorType) => {
-      return (e: Error) => {
-        if (isNetworkError(e)) {
-          console.error(`Uncaught network error: ${e.stack}`);
-          return;
+    if (e instanceof Cancelled) {
+      console.error(`Something was cancelled: ${e.stack}`);
+      return;
+    }
+
+    handle(type, e)
+      .catch(e2 => {
+        // well, we tried.
+        // tslint:disable-next-line
+        console.log(`Error in crash-reporter (${type})\n${e2.stack}`);
+      })
+      .then(() => {
+        if (type === ErrorType.UncaughtException) {
+          os.exit(1);
         }
+      });
+  };
+}
 
-        if (e instanceof Cancelled) {
-          console.error(`Something was cancelled: ${e.stack}`);
-          return;
-        }
-
-        self
-          .handle(type, e)
-          .catch(e2 => {
-            // well, we tried.
-            // tslint:disable-next-line
-            console.log(`Error in crash-reporter (${type})\n${e2.stack}`);
-          })
-          .then(() => {
-            if (type === ErrorType.UncaughtException) {
-              os.exit(1);
-            }
-          });
-      };
-    };
-    process.on("uncaughtException", makeHandler(ErrorType.UncaughtException));
-    process.on("unhandledRejection", makeHandler(ErrorType.UnhandledRejection));
-  },
-};
-
-export default self;
+export default function mountCrashReporter() {
+  process.on("uncaughtException", makeHandler(ErrorType.UncaughtException));
+  process.on("unhandledRejection", makeHandler(ErrorType.UnhandledRejection));
+}
