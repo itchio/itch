8d4dc13dad60e86035f1196ea696a16b0b1713b6
Amos Wenger <amos@itch.io>
2017-06-27 13:31:41 +0200

    stacktrace spec: go one step further and check the presence of lines

--- src/confidence/stacktrace.spec.ts
+++ src/confidence/stacktrace.spec.ts
@@ -2,17 +2,18 @@ import suite from "../test-suite";
 import { spawn } from "child_process";
 
 suite(__filename, s => {
-  s.case("setTimeout", async () => {
+  s.case("setTimeout", async t => {
     const cherrypie = async () => {
       await new Promise((resolve, reject) => setTimeout(resolve, 10));
       throw new Error(`after timeout`);
     };
     const binomial = async () => await cherrypie();
     const abacus = async () => await binomial();
-    await checkStack(s, abacus());
+    const refLine = 12;
+    await checkStack(t, abacus(), refLine - 1, refLine - 2, refLine - 4);
   });
 
-  s.case("spawn", async () => {
+  s.case("spawn", async t => {
     const cherrypie = async () => {
       let e: Error;
       const code = await new Promise<number>((resolve, reject) => {
@@ -23,17 +24,18 @@ suite(__filename, s => {
           resolve(4);
         });
       });
-      s.is(code, 4);
+      t.is(code, 4);
       if (e) {
-        throw e;
+        throw new Error(e.stack);
       }
     };
     const binomial = async () => await cherrypie();
     const abacus = async () => await binomial();
-    await checkStack(s, abacus());
+    const refLine = 34;
+    await checkStack(t, abacus(), refLine - 1, refLine - 2, refLine - 5);
   });
 
-  s.case("after-await", async () => {
+  s.case("after-await", async t => {
     const literallyAnything = async () => null;
     const cherrypie = async () => {
       await literallyAnything();
@@ -41,7 +43,8 @@ suite(__filename, s => {
     };
     const binomial = async () => await cherrypie();
     const abacus = async () => await binomial();
-    await checkStack(s, abacus());
+    const refLine = 46;
+    await checkStack(t, abacus(), refLine - 1, refLine - 2, refLine - 4);
   });
 
   s.case("after-await-thrower", async () => {
@@ -55,19 +58,36 @@ suite(__filename, s => {
     };
     const binomial = async () => await cherrypie();
     const abacus = async () => await binomial();
-    await checkStack(s, abacus());
+    const refLine = 61; // should match with its line number
+    await checkStack(s, abacus(), refLine - 1, refLine - 2, refLine - 4);
   });
 });
 
-async function checkStack(t, promise: Promise<any>) {
+async function checkStack(
+  t,
+  promise: Promise<any>,
+  aLine: number,
+  bLine: number,
+  cLine: number,
+) {
   let threw = false;
   try {
     await promise;
   } catch (e) {
     threw = true;
-    let seen = { a: false, b: false, c: false };
+    let seen = {
+      a: false,
+      b: false,
+      c: false,
+      aLine: false,
+      bLine: false,
+      cLine: false,
+    };
+    const aLinePattern = new RegExp(`\\.ts:${aLine}`);
+    const bLinePattern = new RegExp(`\\.ts:${bLine}`);
+    const cLinePattern = new RegExp(`\\.ts:${cLine}`);
     for (const line of e.stack.split("\n")) {
-      if (/at abacus /.test(line)) {
+      if (/at abacus/.test(line)) {
         seen.a = true;
       }
       if (/at binomial /.test(line)) {
@@ -76,15 +96,34 @@ async function checkStack(t, promise: Promise<any>) {
       if (/at cherrypie /.test(line)) {
         seen.c = true;
       }
+      if (aLinePattern.test(line)) {
+        seen.aLine = true;
+      }
+      if (bLinePattern.test(line)) {
+        seen.bLine = true;
+      }
+      if (cLinePattern.test(line)) {
+        seen.cLine = true;
+      }
     }
 
-    if (!seen.a || !seen.b || !seen.c) {
+    if (
+      !seen.a ||
+      !seen.b ||
+      !seen.c ||
+      !seen.aLine ||
+      !seen.bLine ||
+      !seen.cLine
+    ) {
       // tslint:disable-next-line
       console.log(`Offensive stack trace:\n${e.stack}`);
     }
-    t.true(seen.a, "seen a");
-    t.true(seen.b, "seen b");
-    t.true(seen.c, "seen c");
+    t.true(seen.a, "stack trace contains abacus");
+    t.true(seen.b, "stack trace contains binomial");
+    t.true(seen.c, "stack trace contains cherrypie");
+    t.true(seen.aLine, "stack trace line in abacus");
+    t.true(seen.bLine, "stack trace line in binomial");
+    t.true(seen.cLine, "stack trace line in cherrypie");
   }
 
   if (!threw) {
