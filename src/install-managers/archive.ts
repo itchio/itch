0c69a6267362924bcb18b2346c7b98b837a868e7
Amos Wenger <amos@itch.io>
2017-08-13 23:41:04 +0200

    archive install manager: use bust-ghosts

--- src/install-managers/archive.ts
+++ src/install-managers/archive.ts
@@ -1,83 +1,70 @@
-import butler from "../../util/butler";
-import { extract } from "../../util/extract";
-import deploy2, { IDeployOpts } from "../../util/deploy2";
+import { extract } from "../util/extract";
+import { elapsed } from "../format/datetime";
 
-import { elapsed } from "../../format/datetime";
+import {
+  IInstallManager,
+  IInstallOpts,
+  IUninstallOpts,
+  IInstallResult,
+  IUninstallResult,
+} from "./common/core";
 
-import { IInstallManager } from "./core";
-import getInstallerType from "./get-installer-type";
+import { basename } from "path";
 
-import cullGhosts from "./cull-ghosts";
+import bustGhosts from "./common/bust-ghosts";
 
-async function install(opts: ICoreInstallOpts) {
-  const { ctx, archivePath, destPath, caveId } = opts;
+async function install(opts: IInstallOpts): Promise<IInstallResult> {
+  const { ctx, archivePath, destPath } = opts;
   const logger = opts.logger.child({ name: "archive/install" });
 
-  logger.info(`deploying archive '${archivePath}' to '${destPath}'`);
+  logger.info(`Installing '${basename(archivePath)}'`);
   const startTime = Date.now();
 
-  const deployOpts: IDeployOpts = {
-    ctx,
-    logger,
-    destPath,
-    partialReceipt: {
-      cave: ctx.db.caves.findOneById(caveId),
-    },
-  };
-
-  await deploy2(
-    deployOpts,
-    async () =>
-      await extract({
-        ctx,
-        archivePath: opts.archivePath,
-        destPath,
-        logger,
-      }),
-  );
+  const extractResult = await extract({ ctx, logger, archivePath, destPath });
+  const newFiles = extractResult.files;
+  await bustGhosts({ ctx, logger, destPath, newFiles });
 
   const endTime = Date.now();
-  logger.info(`deployed in ${elapsed(startTime, endTime)}`);
+  logger.info(`Extracted in ${elapsed(startTime, endTime)}`);
 
-  // TODO: restore this
-  // onSingle: async onlyFile => await handleNested(opts, onlyFile),
+  return {
+    files: newFiles,
+  };
 }
 
-async function uninstall(opts: ICoreInstallOpts) {
-  const { ctx, destPath } = opts;
-  const logger = opts.logger.child({ name: "archive/install" });
-
-  logger.info(`wiping directory ${destPath}`);
-  await butler.wipe(destPath, { logger, ctx });
+async function uninstall(opts: IUninstallOpts): Promise<IUninstallResult> {
+  const logger = opts.logger.child({ name: "archive/uninstall" });
+  logger.info(`Nothing to do`);
+  return {};
 }
 
-async function handleNested(opts: ICoreInstallOpts, onlyFile: string) {
-  const { ctx, logger, runtime } = opts;
+// async function handleNested(opts: ICoreInstallOpts, onlyFile: string) {
+//   const { ctx, logger, runtime } = opts;
 
-  // zipped installers need love too
-  const installerName = await getInstallerType({
-    ctx,
-    logger,
-    runtime,
-    target: onlyFile,
-  });
+//   // zipped installers need love too
+//   const installerName = await getInstallerType({
+//     ctx,
+//     logger,
+//     runtime,
+//     target: onlyFile,
+//   });
 
-  if (installerName === "unknown") {
-    return null;
-  }
+//   if (installerName === "unknown") {
+//     return null;
+//   }
 
-  logger.info(`found nested installer '${installerName}': ${onlyFile}`);
-  await coreOperate(
-    {
-      ...opts,
-      archivePath: onlyFile,
-    },
-    "install",
-    installerName,
-  );
+//   logger.info(`found nested installer '${installerName}': ${onlyFile}`);
+//   await coreOperate(
+//     {
+//       ...opts,
+//       archivePath: onlyFile,
+//     },
+//     "install",
+//     installerName,
+//   );
 
-  return { deployed: true };
-}
+//   return { deployed: true };
+// }
 
 const manager: IInstallManager = { install, uninstall };
 export default manager;
