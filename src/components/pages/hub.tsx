26d053375e7cfcc1970df4e4580e1ae2fbfdcbda
Amos Wenger <amos@itch.io>
2017-08-15 01:46:17 +0200

    Remove fade-in animation

--- src/components/pages/hub.tsx
+++ src/components/pages/hub.tsx
@@ -4,7 +4,7 @@ import Sidebar from "../sidebar";
 import SidebarHandle from "../sidebar-handle";
 import HubContent from "../hub-content";
 
-import styled, * as styles from "../styles";
+import styled from "../styles";
 
 const HubPageDiv = styled.div`
   overflow: hidden;
@@ -15,8 +15,6 @@ const HubPageDiv = styled.div`
   right: 0;
   display: flex;
   flex-direction: row;
-
-  animation: ${styles.animations.fadeIn} .4s ease-in;
 `;
 
 class HubPage extends React.PureComponent {
