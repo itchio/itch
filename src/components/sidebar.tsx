itchio/itch

commit 9dd5b45df55fcff27fd550ff607cf7e4342da1e0

Consistent action naming for tab, closes #1387

src/components/sidebar.tsx
@@ -75,7 +75,13 @@ const SortableList = SortableContainer((params: ISortableContainerParams) => {
       {map(items, (id, index) => {
         const active = currentId === id;
         return (
-          <SidebarTab key={id} id={id} active={active} index={index} sortable />
+          <SidebarTab
+            key={id}
+            tab={id}
+            active={active}
+            index={index}
+            sortable
+          />
         );
       })}
     </SortableListContainer>
@@ -131,7 +137,7 @@ class Sidebar extends React.PureComponent<IProps & IDerivedProps, IState> {
             </SidebarHeading>
           </SidebarSection>
           {map(tabs.constant, (id, index) => {
-            return <SidebarTab key={id} id={id} active={currentId === id} />;
+            return <SidebarTab key={id} tab={id} active={currentId === id} />;
           })}
 
           <SidebarSection>
@@ -216,7 +222,7 @@ export default connect<IProps>(injectIntl(Sidebar), {
     fullscreen: (state: IAppState) => state.ui.mainWindow.fullscreen,
     sidebarWidth: (state: IAppState) => state.preferences.sidebarWidth || 240,
     me: (state: IAppState) => state.session.credentials.me,
-    id: (state: IAppState) => state.session.navigation.id,
+    id: (state: IAppState) => state.session.navigation.tab,
     tabs: (state: IAppState) => state.session.navigation.tabs,
   }),
   dispatch: dispatch => ({

src/components/sidebar/tab.tsx
@@ -29,7 +29,7 @@ import { formatDurationAsMessage } from "../../format/datetime";
 
 interface ISortableHubSidebarItemProps {
   props: any & {
-    id: string;
+    tab: string;
   };
 }
 
@@ -41,36 +41,36 @@ const SortableItem = SortableElement((props: ISortableHubSidebarItemProps) => {
 
 class Tab extends React.PureComponent<IProps & IDerivedProps> {
   onClick = () => {
-    const { id, navigate } = this.props;
-    navigate({ id });
+    const { tab, navigate } = this.props;
+    navigate({ tab });
   };
 
   onClose = () => {
-    const { id, closeTab } = this.props;
-    closeTab({ id });
+    const { tab, closeTab } = this.props;
+    closeTab({ tab });
   };
 
   onContextMenu = () => {
-    const { id, openTabContextMenu } = this.props;
-    openTabContextMenu({ id });
+    const { tab, openTabContextMenu } = this.props;
+    openTabContextMenu({ tab });
   };
 
   render() {
-    const { id, index, sortable, data, active, loading } = this.props;
+    const { tab, index, sortable, data, active, loading } = this.props;
 
-    const path = data.path || id;
+    const path = data.path || tab;
     let iconImage = data.iconImage;
     if (/^url/.test(path)) {
       iconImage = data.webFavicon;
     }
 
-    const label = makeLabel(id, data);
+    const label = makeLabel(tab, data);
     const icon = pathToIcon(path);
     let count = 0;
     let progress = 0;
     let sublabel: ILocalizedString = null;
 
-    if (id === "downloads") {
+    if (tab === "downloads") {
       const { downloads } = this.props;
       count = size(getFinishedDownloads(downloads));
       const activeDownload = getActiveDownload(downloads);
@@ -101,7 +101,7 @@ class Tab extends React.PureComponent<IProps & IDerivedProps> {
     }
 
     const props = {
-      id,
+      tab,
       path,
       label,
       icon,
@@ -119,15 +119,15 @@ class Tab extends React.PureComponent<IProps & IDerivedProps> {
     };
 
     if (sortable) {
-      return <SortableItem key={id} index={index} props={props} />;
+      return <SortableItem key={tab} index={index} props={props} />;
     } else {
-      return <Item key={id} {...props} />;
+      return <Item key={tab} {...props} />;
     }
   }
 }
 
 interface IProps {
-  id: string;
+  tab: string;
   index?: number;
   active: boolean;
   sortable?: boolean;

src/actions/navigation-actions.ts
@@ -97,17 +97,17 @@ import {
 } from "../constants/action-types";
 
 const internalNavigate = createAction<INavigatePayload, any>(NAVIGATE);
-export const navigate = (id: any, data = {}, background = false) => {
-  if (typeof id === "object") {
-    return internalNavigate(id);
+export const navigate = (tab: any, data = {}, background = false) => {
+  if (typeof tab === "object") {
+    return internalNavigate(tab);
   } else {
-    return internalNavigate({ id, data, background });
+    return internalNavigate({ tab, data, background });
   }
 };
 
 export const internalOpenTab = createAction<IOpenTabPayload>(OPEN_TAB);
 export const openTab = (payload: IOpenTabPayload) => {
-  return internalOpenTab({ ...payload, id: uuid() });
+  return internalOpenTab({ ...payload, tab: uuid() });
 };
 export const focusTab = createAction<IFocusTabPayload>(FOCUS_TAB);
 export const focusNthTab = createAction<IFocusNthTabPayload>(FOCUS_NTH_TAB);

src/components/browser-meat.tsx
@@ -178,12 +178,12 @@ export class BrowserMeat extends React.PureComponent<
   }
 
   didStartLoading = () => {
-    this.props.tabLoading({ id: this.props.tab, loading: true });
+    this.props.tabLoading({ tab: this.props.tab, loading: true });
     this.updateBrowserState({ loading: true });
   };
 
   didStopLoading = () => {
-    this.props.tabLoading({ id: this.props.tab, loading: false });
+    this.props.tabLoading({ tab: this.props.tab, loading: false });
     this.updateBrowserState({ loading: false });
     this.analyzePage(this.state.browserState.url);
   };
@@ -191,13 +191,13 @@ export class BrowserMeat extends React.PureComponent<
   pageTitleUpdated = (e: any) => {
     // TODO: type
     const { tab, tabDataFetched } = this.props;
-    tabDataFetched({ id: tab, data: { webTitle: e.title } });
+    tabDataFetched({ tab: tab, data: { webTitle: e.title } });
   };
 
   pageFaviconUpdated = (e: any) => {
     // TODO: type
     const { tab, tabDataFetched } = this.props;
-    tabDataFetched({ id: tab, data: { webFavicon: e.favicons[0] } });
+    tabDataFetched({ tab: tab, data: { webFavicon: e.favicons[0] } });
   };
 
   didNavigate = (e: any) => {
@@ -321,9 +321,9 @@ export class BrowserMeat extends React.PureComponent<
           if (parsed.search) {
             newPath += parsed.search;
           }
-          evolveTab({ id: tab, path: newPath });
+          evolveTab({ tab: tab, path: newPath });
         } else {
-          evolveTab({ id: tab, path: `url/${url}` });
+          evolveTab({ tab: tab, path: `url/${url}` });
         }
       },
     );
@@ -497,7 +497,7 @@ export class BrowserMeat extends React.PureComponent<
       wv.reload();
     });
     const { tab, tabReloaded } = this.props;
-    tabReloaded({ id: tab });
+    tabReloaded({ tab: tab });
   };
 
   goBack = () => {

src/components/games.tsx
@@ -45,7 +45,7 @@ class Games extends React.PureComponent<IProps & IDerivedProps> {
     }
 
     this.props.tabParamsChanged({
-      id: tab,
+      tab: tab,
       params: { sortBy, sortDirection },
     });
   };

src/components/meats/all-meats.tsx
@@ -84,7 +84,7 @@ const allTabsSelector = createSelector(
 
 export default connect<IProps>(AllMeats, {
   state: createStructuredSelector({
-    id: (state: IAppState) => state.session.navigation.id,
+    id: (state: IAppState) => state.session.navigation.tab,
     tabs: (state: IAppState) => allTabsSelector(state),
     tabData: (state: IAppState) => state.session.tabData,
   }),

src/components/new-tab.tsx
@@ -114,7 +114,7 @@ export class NewTab extends React.PureComponent<IProps & IDerivedProps> {
             return (
               <NewTabItem
                 key={path}
-                onClick={() => evolveTab({ id: tab, path })}
+                onClick={() => evolveTab({ tab: tab, path })}
               >
                 <Icon icon={icon} />
                 <span>
@@ -177,7 +177,7 @@ export class NewTab extends React.PureComponent<IProps & IDerivedProps> {
 
     const url = await transformUrl(urlField.value);
     const { tab, evolveTab } = this.props;
-    evolveTab({ id: tab, path: `url/${url}` });
+    evolveTab({ tab: tab, path: `url/${url}` });
   };
 }
 

src/components/sidebar/item.tsx
@@ -153,7 +153,7 @@ class Item extends React.PureComponent<IProps, IState> {
   };
 
   render() {
-    const { count, sublabel, progress, id, path, label, active } = this.props;
+    const { count, sublabel, progress, tab, path, label, active } = this.props;
     const { fresh } = this.state;
     const { onClose, onContextMenu } = this.props;
 
@@ -172,7 +172,7 @@ class Item extends React.PureComponent<IProps, IState> {
         onMouseUp={this.onMouseUp}
         onContextMenu={onContextMenu}
         data-path={path}
-        data-id={id}
+        data-id={tab}
       >
         <Row>
           <IconContainer>
@@ -220,7 +220,7 @@ class Item extends React.PureComponent<IProps, IState> {
 
 interface IProps {
   path: string;
-  id: string;
+  tab: string;
   label: ILocalizedString;
   active: boolean;
   count?: number;

commit d434a36541e73f01509e898a408cf7c695e267df

Fix navigation broken by 9dd5b45df55fcff27fd550ff607cf7e4342da1e0

src/components/sidebar.tsx
@@ -68,16 +68,16 @@ const SortableListContainer = styled.div`overflow-y: auto;`;
 
 const SortableList = SortableContainer((params: ISortableContainerParams) => {
   const { sidebarProps, items } = params;
-  const currentId = sidebarProps.id;
+  const currentTab = sidebarProps.tab;
 
   return (
     <SortableListContainer>
-      {map(items, (id, index) => {
-        const active = currentId === id;
+      {map(items, (tab, index) => {
+        const active = currentTab === tab;
         return (
           <SidebarTab
-            key={id}
-            tab={id}
+            key={tab}
+            tab={tab}
             active={active}
             index={index}
             sortable
@@ -118,7 +118,7 @@ class Sidebar extends React.PureComponent<IProps & IDerivedProps, IState> {
       osx,
       sidebarWidth,
       fullscreen,
-      id: currentId,
+      tab: currentId,
       tabs,
     } = this.props;
 
@@ -189,7 +189,7 @@ interface IDerivedProps {
   fullscreen: boolean;
   me: IOwnUser;
 
-  id: string;
+  tab: string;
   path: string;
   tabs: {
     constant: string[];
@@ -222,7 +222,7 @@ export default connect<IProps>(injectIntl(Sidebar), {
     fullscreen: (state: IAppState) => state.ui.mainWindow.fullscreen,
     sidebarWidth: (state: IAppState) => state.preferences.sidebarWidth || 240,
     me: (state: IAppState) => state.session.credentials.me,
-    id: (state: IAppState) => state.session.navigation.tab,
+    tab: (state: IAppState) => state.session.navigation.tab,
     tabs: (state: IAppState) => state.session.navigation.tabs,
   }),
   dispatch: dispatch => ({

src/components/sidebar/tab.tsx
@@ -147,12 +147,13 @@ interface IDerivedProps {
 
 export default connect<IProps>(injectIntl(Tab), {
   state: (initialState, initialProps) => {
-    let { id } = initialProps;
+    let { tab } = initialProps;
 
     return createStructuredSelector({
-      data: (state: IAppState) => state.session.tabData[id] || emptyObj,
-      loading: (state: IAppState) => !!state.session.navigation.loadingTabs[id],
-      downloads: (state: IAppState) => id === "downloads" && state.downloads,
+      data: (state: IAppState) => state.session.tabData[tab] || emptyObj,
+      loading: (state: IAppState) =>
+        !!state.session.navigation.loadingTabs[tab],
+      downloads: (state: IAppState) => tab === "downloads" && state.downloads,
     });
   },
   dispatch: dispatch => ({