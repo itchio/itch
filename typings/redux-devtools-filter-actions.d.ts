c65fc93f40928fda996c0110ad69dddaff612e55
Amos Wenger <amos@itch.io>
2017-08-26 01:31:07 +0200

    Remove a bunch more unused packages

--- typings/redux-devtools-filter-actions.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Typings for https://github.com/zalmoxisus/redux-devtools-filter-actions
- */
-declare module "redux-devtools-filter-actions" {
-  import * as React from "react";
-  class FilterMonitor extends React.PureComponent<any, any> {}
-  export default FilterMonitor;
-}
