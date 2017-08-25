9a6b263e0e2e2ba7ab19771f59b5714725cc0e7b
Amos Wenger <amos@itch.io>
2017-08-26 01:34:11 +0200

    remove react-timeago-titlefix

--- typings/react-timeago-titlefix.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Typings for our fork of https://github.com/naman34/react-timeago
- */
-declare module "react-timeago-titlefix" {
-  import * as React from "react";
-  class TimeAgo extends React.PureComponent<any, any> {}
-  export default TimeAgo;
-}
