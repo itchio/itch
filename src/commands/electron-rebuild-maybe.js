1799dd3cd70cfa328f8cc71a0a998b58ba0136d5
Amos Wenger <amos@itch.io>
2017-09-29 14:10:58 +0200

    Closes #1536

--- src/commands/electron-rebuild-maybe.js
+++ src/commands/electron-rebuild-maybe.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 
+// Only run electron-rebuild if electron is installed
 try {
-  require("child_process").execSync("electron-rebuild");
+  require("fs").statSync("./node_modules/electron/package.json");
 } catch (e) {
-  console.warn(`Warning: couldn't run electron-rebuild: ${e.stack}`);
+  if (e.code === "ENOENT") {
+    console.log(`No devDependencies, not running electron-rebuild`);
+    process.exit(0);
+  } else {
+    console.error(`While deciding whether or not to run electron-rebuild: ${e.stack}`);
+    process.exit(1);
+  }
 }
+
+require("child_process").execSync("electron-rebuild");
