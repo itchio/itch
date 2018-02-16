1a346d06d7cf9106eae02a0f6c8cf3057650e0b6
Amos Wenger <amos@itch.io>
2018-02-16 13:23:36 +0100

    Make sure we never require development react from the metal side

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,15 @@
 const isDev = require("electron-is-dev");
 const isCanary = require("../package.json").name === "kitch";
 
+const envName =
+  process.env.ITCH_APP_ENV ||
+  process.env.NODE_ENV ||
+  (isDev ? "development" : "production");
+
+process.env.NODE_ENV = envName;
+
 export default {
-  name:
-    process.env.ITCH_APP_ENV ||
-    process.env.NODE_ENV ||
-    (isDev ? "development" : "production"),
+  name: envName,
   channel: isCanary ? "canary" : "stable",
   appName: isCanary ? "kitch" : "itch",
   unitTests: false,
