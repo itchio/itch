1511585939a7433702201f646fb32730e18ce98a
Amos Wenger <amos@itch.io>
2018-03-14 13:46:43 +0100

    Reimplement search on top of buse

--- src/reactors/search/index.ts
+++ src/reactors/search/index.ts
@@ -1,12 +1,41 @@
 import { Watcher } from "../watcher";
 
-import rootLogger from "../../logger";
-const logger = rootLogger.child({ name: "fetch-search" });
-
 import { actions } from "../../actions";
+import { call, messages } from "../../buse/index";
+import { ISearchResults } from "../../types/index";
+import { mergeGames, mergeUsers } from "./search-helpers";
 
 export default function(watcher: Watcher) {
   watcher.on(actions.search, async (store, action) => {
-    logger.error("TODO: re-implement search with buse");
+    const profileId = store.getState().profile.credentials.me.id;
+    const { query } = action.payload;
+    if (query.length < 3) {
+      return;
+    }
+
+    let results: ISearchResults = {};
+
+    let dispatch = () => {
+      store.dispatch(actions.searchFetched({ query, results }));
+    };
+
+    let promises = [];
+    promises.push(
+      call(messages.SearchGames, { profileId, query }, client => {
+        // TODO: give params directly to request handlers
+        client.onNotification(messages.SearchGamesYield, ({ params }) => {
+          results = mergeGames(results, params.games);
+          dispatch();
+        });
+      })
+    );
+    promises.push(messages.SearchUsers, { profileId, query }, client => {
+      client.onNotification(messages.SearchUsers, ({ params }) => {
+        results = mergeUsers(results, params.users);
+        dispatch();
+      });
+    });
+
+    await Promise.all(promises);
   });
 }
