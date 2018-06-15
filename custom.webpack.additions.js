ff89353f562a062ab722cc865fc3333a8e31156f
Amos Wenger <amos@itch.io>
2018-06-15 22:13:18 +0200

    Use hard-source-webpack-plugin which makes startup 90% faster?

--- custom.webpack.additions.js
+++ custom.webpack.additions.js
@@ -1,7 +1,11 @@
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
 
 module.exports = {
   resolve: {
     plugins: [new TsconfigPathsPlugin({})],
-  }
+  },
+  plugins: [
+    new HardSourceWebpackPlugin(),
+  ],
 };
