d95a90b96cb55555810b4f656d05b03397c4414f
Amos Wenger <amoswenger@gmail.com>
2018-07-01 17:48:10 +0200

    Working webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,89 @@
+const { HotModuleReplacementPlugin } = require("webpack");
+
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
 const HappyPack = require("happypack");
+const WebpackBuildNotifierPlugin = require("webpack-build-notifier");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const path = require("path");
 const merge = require("webpack-merge");
 
-module.exports = [
-  merge.smart(getCommonConfig(), {
-    target: "electron-main",
-    entry: "./src/main/index.ts",
-    output: {
-      filename: "main.js"
-    },
-    externals: [
-      "electron",
-      "asfw",
-    ],
-  }),
-  merge.smart(getCommonConfig(), {
-    target: "electron-renderer",
-    entry: "./src/renderer/index.tsx",
-    output: {
-      "filename": "renderer.js"
-    },
-    externals: [
-      "electron",
-      "systeminformation",
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.(png|svg|woff|woff2)$/,
-          use: [
-            {
-              loader: "file-loader"
-            }
-          ]
-        },
-        {
-          test: /\.css$/,
-          use: [
-            { loader: "style-loader" },
-            { loader: "css-loader" },
-          ]
-        }
+module.exports = (env) => {
+  return [
+    merge.smart(getCommonConfig("main", env), {
+      target: "electron-main",
+      entry: {
+        main: "./src/main/index.ts",
+        "inject-game": "./src/main/inject/inject-game.ts",
+        "inject-captcha": "./src/main/inject/inject-captcha.ts",
+      },
+      externals: [
+        "bindings"
+      ],
+      plugins: [
+        new WebpackBuildNotifierPlugin({
+          title: "itch (main)",
+        }),
+      ],
+    }),
+    merge.smart(getCommonConfig("renderer", env), {
+      target: "electron-renderer",
+      entry: {
+        renderer: "./src/renderer/index.tsx",
+      },
+      externals: [
+        "systeminformation",
+      ],
+      module: {
+        rules: [
+          {
+            test: /\.(png|svg|woff|woff2)$/,
+            use: [
+              { loader: "file-loader" },
+            ],
+          },
+          {
+            test: /\.css$/,
+            use: [
+              { loader: "style-loader" },
+              { loader: "css-loader" },
+            ],
+          }
+        ]
+      },
+      plugins: [
+        new HotModuleReplacementPlugin(),
+        new WebpackBuildNotifierPlugin({
+          title: "itch (renderer)",
+        }),
+        new HtmlWebpackPlugin({
+          filename: "index.html",
+          template: path.resolve(`./src/index.ejs`),
+          minify: false,
+        }),
       ]
-    }
-  }),
-];
+    }),
+  ]
+};
 
-function getCommonConfig () {
+function getCommonConfig(type, env) {
+  const isProduction = env.mode === "production";
+  const mode = isProduction ? "production" : "development";
+
   return {
-    mode: "development",
+    mode,
     devtool: "eval",
+    node: {
+      __dirname: !isProduction,
+      __filename: !isProduction,
+    },
+    output: {
+      filename: "[name].js",
+      chunkFilename: "[name].bundle.js",
+      libraryTarget: "commonjs2",
+      path: path.resolve(`./dist/${type}`),
+    },
     resolve: {
       extensions: [".ts", ".tsx", ".js"],
       plugins: [
@@ -64,11 +96,15 @@ function getCommonConfig () {
           test: /\.tsx?$/,
           exclude: "/node_modules/",
           use: [
-            {
-              loader: "happypack/loader?id=ts",
-            }
+            { loader: "happypack/loader?id=ts" },
           ]
         },
+        {
+          test: /\.node$/,
+          use: [
+            { loader: "node-loader" },
+          ],
+        }
       ]
     },
     plugins: [
@@ -84,5 +120,9 @@ function getCommonConfig () {
         ]
       }),
     ],
+    optimization: {
+      minimize: false,
+      minimizer: [],
+    },
   }
 }
