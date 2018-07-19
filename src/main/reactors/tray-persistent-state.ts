00770144fa796cb21030c45c86fceadf3a16712a
Amos Wenger <amoswenger@gmail.com>
2018-07-19 16:14:04 +0200

    Closes #1106 (also change back to the colorful tray icon)

--- src/main/reactors/tray-persistent-state.ts
+++ src/main/reactors/tray-persistent-state.ts
@@ -1,9 +1,10 @@
 import { Store, Action } from "common/types";
-import { Tray } from "electron";
+import { Tray, nativeImage } from "electron";
 import { getImagePath } from "common/util/resources";
 import env from "common/env";
 import { actions } from "common/actions";
 import { release } from "os";
+import { mainLogger } from "main/logger";
 
 let tray: Electron.Tray;
 
@@ -12,24 +13,28 @@ let lastNotificationAction: Action<any>;
 
 export function getTray(store: Store): Electron.Tray {
   if (!tray) {
-    // cf. https://github.com/itchio/itch/issues/462
-    // windows still displays a 16x16, whereas
-    // some linux DEs don't know what to do with a @x2, etc.
-    let suffix = "";
-    if (process.platform !== "linux") {
-      suffix = "-small";
+    const iconPath = getImagePath(`tray/${env.appName}.png`);
+    mainLogger.info(`Using tray image (${iconPath})`);
+    let iconImage = nativeImage.createFromPath(iconPath);
+    let onKDE = process.env.XDG_CURRENT_DESKTOP === "KDE";
+
+    if (process.platform === "win32") {
+      // cf. https://github.com/itchio/itch/issues/462
+      // windows still displays a 16x16, whereas
+      // some linux DEs don't know what to do with a @x2, etc.
+      iconImage = iconImage.resize({
+        width: 16,
+        height: 16,
+      });
+    } else if (onKDE) {
+      // KDE can't handle a 256x256 png apparently
+      iconImage = iconImage.resize({
+        width: 24,
+        height: 24,
+      });
     }
 
-    let base = "white";
-    if (process.platform === "win32" && !/^10\./.test(release())) {
-      // windows older than 10 get the old colorful tray icon
-      base = env.appName;
-    }
-
-    const iconName = `${base}${suffix}.png`;
-    const iconPath = getImagePath("tray/" + iconName);
-
-    tray = new Tray(iconPath);
+    tray = new Tray(iconImage);
     tray.setToolTip(env.appName);
     tray.on("click", () => {
       store.dispatch(actions.focusWind({ wind: "root", toggle: true }));
