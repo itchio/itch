a28630511c2351406d4fc0133323afa5e731b7d6
Amos Wenger <amoswenger@gmail.com>
2019-07-04 20:42:23 +0200

    Turn integration-tests into a go module, fix the way it's built/ran

---
+++ integration-tests/go.mod
@@ -0,0 +1,13 @@
+module github.com/itchio/itch/integration-tests
+
+go 1.12
+
+require (
+	github.com/fasterthanlime/go-selenium v0.1.1-0.20180614144419-1da511db778f
+	github.com/hpcloud/tail v1.0.0
+	github.com/onsi/gocleanup v0.0.0-20140331211545-c1a5478700b5
+	github.com/pkg/errors v0.8.1
+	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
--- release/ci-integration-tests.js
+++ release/ci-integration-tests.js
@@ -3,16 +3,14 @@
 // run integration tests
 
 const $ = require("./common");
-const path = require("path").posix;
 
 async function main() {
-  process.env.GOPATH = path.join(process.cwd(), "gopath");
-  const target = "github.com/itchio/itch-integration-tests";
-  $(await $.sh(`rm -rf gopath/src/${target}`));
-  $(await $.sh(`mkdir -p gopath/src/${target}`));
-  $(await $.sh(`cp -rf integration-tests/* gopath/src/${target}`));
-  $(await $.sh(`go get -v ${target}`));
-  $(await $.sh(`gopath/bin/itch-integration-tests`));
+  let binName = `itch-integration-tests`;
+  await $.cd("integration-tests", async () => {
+    $(await $.sh(`go build -o runner -v`));
+  });
+  process.env.ELECTRON_DISABLE_SANDBOX = "1";
+  $(await $.sh(`./integration-tests/runner`));
   return;
 }
 
