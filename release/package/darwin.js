ae07123c9c2c7116257e078d8ea37d9e5e94d41f
Amos Wenger <amoswenger@gmail.com>
2019-10-24 10:21:18 +0200

    notarizing: measure & log more stuff

--- release/package/darwin.js
+++ release/package/darwin.js
@@ -11,15 +11,23 @@ module.exports = {
         `codesign --deep --force --verbose --timestamp --sign "${signKey}" ${appBundle}`
       )
     );
+
+    $.say("Verifying signature...");
     $(await $.sh(`codesign --verify -vvvv ${appBundle}`));
     $(await $.sh(`spctl -a -vvvv ${appBundle}`));
-    const { notarize } = require("electron-notarize");
-    await notarize({
-      appBundleId: $.appBundleId(),
-      appPath: appBundle,
-      appleId: "amoswenger@gmail.com",
-      appleIdPassword: process.env.APPLE_ID_PASSWORD,
-    });
+
+    $.say("Notarizing...");
+    await $.measure("electron-notarize", async () => {
+      const { notarize } = require("electron-notarize");
+      await notarize({
+        appBundleId: $.appBundleId(),
+        appPath: appBundle,
+        appleId: "amoswenger@gmail.com",
+        appleIdPassword: process.env.APPLE_ID_PASSWORD,
+      });
+    })
+
+    $.say("Testing notarized requirement...");
     $(await $.sh(`codesign --test-requirement="=notarized" -vvvv ${appBundle}`));
   },
 };
