18b154c1690493b2c0e6658738d0bf4b90abb14d
Amos Wenger <amoswenger@gmail.com>
2020-02-24 09:41:50 -0800

    Add entitlements, signed application runs

--- release/packaging/darwin.js
+++ release/packaging/darwin.js
@@ -47,6 +47,10 @@ module.exports = {
       $(await $.sh(`codesign --sign "${identity}" --force --timestamp --options runtime "${binary}"`));
     }
 
+    $.say("Writing entitlements file");
+    const entitlementsPath = ospath.join(".", "entitlements.plist");
+    fs.writeFileSync(entitlementsPath, entitlements());
+
     $.say("Signing Application bundle...");
     await $.measure("electron-osx-sign", async () => {
       require("debug").enable("electron-osx-sign");
@@ -55,6 +59,8 @@ module.exports = {
         app: appBundle,
         hardenedRuntime: true,
         identity,
+        entitlements: entitlementsPath,
+        "entitlements-inherit": entitlementsPath,
         platform: "darwin",
         version: cx.electronVersion,
       });
@@ -64,21 +70,42 @@ module.exports = {
     $(await $.sh(`codesign --verify -vvvv ${appBundle}`));
     $(await $.sh(`spctl -a -vvvv ${appBundle}`));
 
-    $.say("Notarizing...");
-    await $.measure("electron-notarize", async () => {
-      require("debug").enable("electron-notarize");
-      const { notarize } = require("electron-notarize");
-      await notarize({
-        appBundleId: $.appBundleId(),
-        appPath: appBundle,
-        appleId: "amoswenger@gmail.com",
-        appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    if (process.env.SKIP_NOTARIZE) {
+      $.say(`$SKIP_NOTARIZE is set, skipping notarization...`);
+    } else {
+      $.say("Notarizing...");
+      await $.measure("electron-notarize", async () => {
+        require("debug").enable("electron-notarize");
+        const { notarize } = require("electron-notarize");
+        await notarize({
+          appBundleId: $.appBundleId(),
+          appPath: appBundle,
+          appleId: "amoswenger@gmail.com",
+          appleIdPassword: process.env.APPLE_ID_PASSWORD,
+        });
       });
-    });
 
-    $.say("Testing notarized requirement...");
-    $(
-      await $.sh(`codesign --test-requirement="=notarized" -vvvv ${appBundle}`)
-    );
+      $.say("Testing notarized requirement...");
+      $(
+        await $.sh(`codesign --test-requirement="=notarized" -vvvv ${appBundle}`)
+      );
+    }
   },
 };
+
+function entitlements() {
+  return `
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>com.apple.security.cs.allow-unsigned-executable-memory</key>
+    <true/>
+    <key>com.apple.security.network.client</key>
+    <true/>
+    <key>com.apple.security.files.user-selected.read-write</key>
+    <true/>
+  </dict>
+</plist>
+`;
+}
