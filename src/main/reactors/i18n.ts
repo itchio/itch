96735f27b24616c5bf47299d96a0cd51607aa462
Amos Wenger <amoswenger@gmail.com>
2020-08-10 10:01:13 +0200

    log language settings in i18n reactor

--- src/main/reactors/i18n.ts
+++ src/main/reactors/i18n.ts
@@ -4,7 +4,10 @@ import { createSelector } from "reselect";
 import { RootState } from "common/types";
 import { actions } from "common/actions";
 
+import { mainLogger } from "main/logger";
+
 const fallbackLang = "en";
+const logger = mainLogger.child(__filename);
 
 export default function(watcher: Watcher) {
   watcher.onStateChange({
@@ -14,6 +17,9 @@ export default function(watcher: Watcher) {
         (rs: RootState) => rs.preferences.lang,
         (sniffedLang, preferenceLang) => {
           const lang = preferenceLang || sniffedLang || fallbackLang;
+          logger.info(
+            `Language settings: preference ${preferenceLang}, sniffed ${sniffedLang}, fallback ${fallbackLang}`
+          );
           schedule.dispatch(actions.languageChanged({ lang }));
         }
       ),
