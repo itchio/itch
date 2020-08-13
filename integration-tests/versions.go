717c68ddf713d96d3f50e4faeb7bf350c91c775c
Amos Wenger <amoswenger@gmail.com>
2020-08-13 11:43:20 +0200

    pin electron version, bump chrome driver version

--- integration-tests/versions.go
+++ integration-tests/versions.go
@@ -1,4 +1,4 @@
 package main
 
-const electronVersion = "9.1.2"
-const chromeDriverVersionString = "ChromeDriver 83.0.4103.104"
+const electronVersion = "9.2.0"
+const chromeDriverVersionString = "ChromeDriver 83.0.4103.122"
