eba27ded0b2d89e8e3c3e80f2715100b0b0e37c3
Amos Wenger <amoswenger@gmail.com>
2020-08-19 12:50:19 +0200

    fix proxy settings adjustments

--- src/main/reactors/proxy.ts
+++ src/main/reactors/proxy.ts
@@ -3,35 +3,20 @@ import { actions } from "common/actions";
 
 import { ProxySettings } from "common/types";
 import { partitionForUser } from "common/util/partition-for-user";
+import { session, Session } from "electron";
 
 export default function (watcher: Watcher) {
   watcher.on(actions.loginSucceeded, async (store, action) => {
     const userId = action.payload.profile.user.id;
 
-    const { session } = require("electron");
     const partition = partitionForUser(String(userId));
     const ourSession = session.fromPartition(partition, { cache: true });
     await applyProxySettings(ourSession, store.getState().system);
   });
 }
 
-interface ProxyConfig {
-  /** The URL associated with the PAC file. */
-  pacScript: string;
-  /** Rules indicating which proxies to use. */
-  proxyRules: string;
-  /** Rules indicating which URLs should bypass the proxy settings. */
-  proxyBypassRules: string;
-}
-
-/** Something that accepts electron proxy settings - usually a session */
-interface AcceptsProxyConfig {
-  setProxy(config: ProxyConfig, callback: Function): void;
-  enableNetworkEmulation(options: { offline?: boolean }): void;
-}
-
 export async function applyProxySettings(
-  session: AcceptsProxyConfig,
+  session: Session,
   system: ProxySettings
 ) {
   if (process.env.ITCH_EMULATE_OFFLINE === "1") {
@@ -47,18 +32,9 @@ export async function applyProxySettings(
 
   const proxyRules = system.proxy;
 
-  return await new Promise<void>((resolve, reject) => {
-    session.setProxy(
-      {
-        pacScript: null,
-        proxyRules,
-        proxyBypassRules: null,
-      },
-      resolve
-    );
-
-    setTimeout(function () {
-      reject(new Error("proxy settings adjustment timed out"));
-    }, 1000);
+  await session.setProxy({
+    pacScript: null,
+    proxyRules,
+    proxyBypassRules: null,
   });
 }
