cac4dc665df6a88f79f51386a6d0dce02220538a
Amos Wenger <amoswenger@gmail.com>
2020-08-27 16:21:58 +0200

    More Electron typings mischief

--- src/common/util/net.ts
+++ src/common/util/net.ts
@@ -1,5 +1,5 @@
 export function getResponseHeader(
-  responseHeaders: Record<string, string[]> | undefined,
+  responseHeaders: Record<string, string | string[]> | undefined,
   headerName: string
 ): string | null {
   if (!responseHeaders) {
@@ -7,10 +7,12 @@ export function getResponseHeader(
   }
 
   let value = responseHeaders[headerName];
+  // `string` type
   if (typeof value === "string") {
     return value;
   }
 
+  // `string[]` type
   if (typeof value === "object" && typeof value.length === "number") {
     return value[0];
   }
