f028981fe2aab7d353e27d349688f6581eb48f81
leaf corcoran <leafot@gmail.com>
2020-10-28 10:54:33 -0700

    have sign tool pick the best cert

--- release/packaging/windows.js
+++ release/packaging/windows.js
@@ -25,7 +25,7 @@ async function sign(cx, packageDir) {
 
   // forward-slashes are doubled because of mingw, see http://www.mingw.org/wiki/Posix_path_conversion
   let signParams =
-    '//v //s MY //n "itch corp." //fd sha256 //tr http://timestamp.comodoca.com/?td=sha256 //td sha256';
+    '//v //s MY //n "itch corp." //fd sha256 //tr http://timestamp.comodoca.com/?td=sha256 //td sha256 //a';
   let signtoolPath = "vendor/signtool.exe";
 
   $(`${signtoolPath} sign ${signParams} "${exePath}"`);
