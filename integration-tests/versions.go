3bb242387795317cea0ff1d6cb751ba64ed1abd7
leaf corcoran <leafot@gmail.com>
2022-03-12 12:07:11 -0800

    start update to electron 11 #2771

--- integration-tests/versions.go
+++ integration-tests/versions.go
@@ -1,4 +1,4 @@
 package main
 
-const electronVersion = "10.4.7"
-const chromeDriverVersionString = "ChromeDriver 85.0.4183.121"
+const electronVersion = "11.5.0"
+const chromeDriverVersionString = "ChromeDriver 87.0.4280.141"
