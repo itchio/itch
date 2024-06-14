539636b08c7a2e75a75c6701f17e2a480743b5c9
Stephen Altamirano <s@stvs.tv>
2024-06-14 13:32:36 -0700

    Escape game titles in context menus (Fixes #2890)
    Ampersands in context menus get interpreted as a shortcut
    definition. This escapes game titles before placing them into a
    context menu payload.
    
    Co-Authored-By: Luke <jls47@users.noreply.github.com>

--- src/main/reactors/context-menu/build-template.ts
+++ src/main/reactors/context-menu/build-template.ts
@@ -116,7 +116,7 @@ export function gameControls(store: Store, game: Game): MenuTemplate {
       statusItems.push({
         localizedLabel: [
           `grid.item.${mainAction}_title`,
-          { title: game.title },
+          { title: escapeForContextMenu(game.title) },
         ],
         action: actions.queueGame({ game }),
       });
@@ -178,7 +178,10 @@ export function gameControls(store: Store, game: Game): MenuTemplate {
       } else {
         // we have any kind of access
         statusItems.push({
-          localizedLabel: ["grid.item.install_title", { title: game.title }],
+          localizedLabel: [
+            "grid.item.install_title",
+            { title: escapeForContextMenu(game.title) },
+          ],
           action: actions.queueGame({ game }),
         });
       }
@@ -264,4 +267,10 @@ export function userMenu(store: Store): MenuTemplate {
       accelerator: "CmdOrCtrl+Q",
     },
   ];
+}
+
+function escapeForContextMenu(label: string) {
+  // In a context menu, '&[^&]' will be interpreted as a shortcut
+  // definition. Escaping requires a second ampersand
+  return label.replaceAll("&", "&&");
 }
