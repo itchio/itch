6dc1f3ed149e9a27d06c323557b905e336761fb7
fabrb <faiorb@outlook.com>
2025-02-15 17:14:15 -0300

    :clap: Change captcha modal

--- src/main/reactors/login.ts
+++ src/main/reactors/login.ts
@@ -66,7 +66,7 @@ export default function (watcher: Watcher) {
                   widgetParams: {
                     url: recaptchaUrl || urls.itchio + "/captcha",
                   },
-                  fullscreen: true,
+                  fullscreen: false,
                 })
               );
 
