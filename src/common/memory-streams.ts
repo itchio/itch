a6c8720d606349203342e26e203fe717f28a98f1
leaf corcoran <leafot@gmail.com>
2026-02-20 16:51:04 -0800

    attempt to fix log entries not showing up correctly in game crash dialog

--- src/common/memory-streams.ts
+++ src/common/memory-streams.ts
@@ -1,11 +1,16 @@
 import { Writable, WritableOptions } from "readable-stream";
 
 export class WritableMemoryStream extends Writable {
+  private chunks: Buffer[] = [];
+
   constructor(options?: WritableOptions) {
     super(options);
   }
 
   _write(chunk: Buffer, encoding: string, callback: () => void): void {
+    this.chunks.push(
+      Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk, encoding)
+    );
     callback();
   }
 
@@ -14,10 +19,6 @@ export class WritableMemoryStream extends Writable {
   }
 
   toBuffer(): Buffer {
-    const buffers: Buffer[] = [];
-    this._writableState.buffer.forEach((data: { chunk: Buffer }) => {
-      buffers.push(data.chunk);
-    });
-    return Buffer.concat(buffers);
+    return Buffer.concat(this.chunks);
   }
 }
