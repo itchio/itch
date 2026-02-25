3a9c33a654e55e039bc0ae5155d83fb0ddd1aca2
leaf corcoran <leafot@gmail.com>
2026-02-24 23:16:25 -0800

    fix how active attribute works

--- src/renderer/pages/PreferencesPage/Label.tsx
+++ src/renderer/pages/PreferencesPage/Label.tsx
@@ -1,11 +1,18 @@
 import styled, * as styles from "renderer/styles";
 import classNames from "classnames";
 
-const Label = styled.label.attrs(
-  (props: { active?: boolean; className?: string }) => ({
-    className: classNames(props.className, { active: props.active }),
+interface LabelProps {
+  active?: boolean;
+  className?: string;
+}
+
+const Label = styled.label
+  .withConfig({
+    shouldForwardProp: (prop: string) => prop !== "active",
   })
-)`
+  .attrs((props: LabelProps) => ({
+    className: classNames(props.className, { active: props.active }),
+  }))<LabelProps>`
   background: ${(props) => props.theme.explanation};
   padding: 8px 11px;
   font-size: 14px;
