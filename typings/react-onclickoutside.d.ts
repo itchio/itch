06d2c46adc419c6f2a21fceda091e54a31c7adfd
leaf corcoran <leafot@gmail.com>
2026-08-15 19:13:42 -0700

    remove some dead dependencies

--- typings/react-onclickoutside.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Typings for https://github.com/Pomax/react-onclickoutside
- */
-declare module "react-onclickoutside" {
-  import React from "react";
-
-  function onClickOutside<T>(component: T): T;
-  export = onClickOutside;
-}
